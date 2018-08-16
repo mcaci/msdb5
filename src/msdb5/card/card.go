8e0573652836e427f8840fbc2406d1dc39044681
Michele Caci <michele.caci@amadeus.com>
2018-08-16 16:21:30 +0200

    Refactoring card

--- src/msdb5/card/card.go
+++ src/msdb5/card/card.go
@@ -1,5 +1,7 @@
 package card
 
+import "errors"
+
 // Card type
 type Card struct {
 	number uint8
@@ -7,10 +9,17 @@ type Card struct {
 }
 
 // ByID func
-func ByID(id int) *Card {
-	a := uint8((id + 1) % 10)
-	b := Seed((id - 1) / 10)
-	return &Card{number: a, seed: b}
+func ByID(id int) (*Card, error) {
+	if id < 1 {
+		return nil, errors.New("Index cannot be less than 1")
+	} else if id > 40 {
+		return nil, errors.New("Index cannot be more than 40")
+	} else {
+		seedIndex := (id - 1) / 10
+		number := uint8(id - (10 * seedIndex))
+		seed := Seed(seedIndex)
+		return &Card{number: number, seed: seed}, nil
+	}
 }
 
 func (card *Card) points() uint8 {
--- src/msdb5/card/card_test.go
+++ src/msdb5/card/card_test.go
@@ -2,22 +2,71 @@ package card
 
 import "testing"
 
-func TestId0IsAceOfCoin(t *testing.T) {
-	card := ByID(1)
-	if card.number != 1 {
-		t.Fatalf("Card %v is not an ace", card)
+func TestId0IsInvalid(t *testing.T) {
+	_, err := ByID(0)
+	if err == nil {
+		t.Fatal("0 is not valid id")
+	}
+}
+func TestId41IsInvalid(t *testing.T) {
+	_, err := ByID(41)
+	if err == nil {
+		t.Fatal("41 is not valid id")
 	}
 }
 
-// func TestId39IsKingOfSomething(t *testing.T) {
-// 	card := ByID(39)
-// 	if card.number != 9 {
-// 		t.Fatalf("Card %v is not a nine", card)
-// 	}
-// }
+func TestId1IsAce(t *testing.T) {
+	card, _ := ByID(1)
+	if card.number != 1 {
+		t.Fatalf("Card %v's number is not ace", card)
+	}
+}
 
-// func ByID(id int) *Card {
-// 	a := uint8(id % 10)
-// 	b := Seed(id / 10)
-// 	return &Card{number: a, seed: b}
-// }
+func TestId1IsCoin(t *testing.T) {
+	card, _ := ByID(1)
+	if card.seed != Coin {
+		t.Fatalf("Card %v's seed is not coin", card)
+	}
+}
+
+func TestId12IsTwo(t *testing.T) {
+	card, _ := ByID(12)
+	if card.number != 2 {
+		t.Fatalf("Card %v's number is not two", card)
+	}
+}
+
+func TestId12IsCup(t *testing.T) {
+	card, _ := ByID(12)
+	if card.seed != Cup {
+		t.Fatalf("Card %v's seed is not cup", card)
+	}
+}
+
+func TestId25IsFive(t *testing.T) {
+	card, _ := ByID(25)
+	if card.number != 5 {
+		t.Fatalf("Card %v's number is not five", card)
+	}
+}
+
+func TestId25IsSword(t *testing.T) {
+	card, _ := ByID(25)
+	if card.seed != Sword {
+		t.Fatalf("Card %v's seed is not sword", card)
+	}
+}
+
+func TestId40IsKing(t *testing.T) {
+	card, _ := ByID(40)
+	if card.number != 10 {
+		t.Fatalf("Card %v's number is not king", card)
+	}
+}
+
+func TestId40IsCudgel(t *testing.T) {
+	card, _ := ByID(40)
+	if card.seed != Cudgel {
+		t.Fatalf("Card %v's seed is not cudgel", card)
+	}
+}
