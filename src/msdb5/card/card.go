3d7646ba3f51bb372a184f346925a8755f3aa3ff
Michele Caci <michele.caci@amadeus.com>
2018-08-16 14:37:44 +0200

    Separate Stringer implementation in Card

--- src/msdb5/card/card.go
+++ src/msdb5/card/card.go
@@ -1,7 +1,5 @@
 package card
 
-import "strconv"
-
 // Card type
 type Card struct {
 	number uint8
@@ -10,8 +8,8 @@ type Card struct {
 
 // ByID func
 func ByID(id int) *Card {
-	a := uint8(id % 10)
-	b := Seed(id / 10)
+	a := uint8((id + 1) % 10)
+	b := Seed((id - 1) / 10)
 	return &Card{number: a, seed: b}
 }
 
@@ -30,8 +28,4 @@ func (card *Card) points() uint8 {
 	default:
 		return 0
 	}
-}
-
-func (card Card) String() string {
-	return "(" + strconv.Itoa(int(card.number)) + " of " + card.seed.String() + ")"
 }
