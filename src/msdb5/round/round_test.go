98b41a037f7cbeb1f81fa19040f169a03b1e5ce0
Michele Caci <michele.caci@amadeus.com>
2018-08-21 14:57:49 +0200

    Refactor: extract method in round test

--- src/msdb5/round/round_test.go
+++ src/msdb5/round/round_test.go
@@ -3,14 +3,7 @@ package round
 import "testing"
 import "msdb5/card"
 
-func TestScenario1WithAceOfCoinWinning(t *testing.T) {
-	first, err1 := card.ByID(1)  // 1 Coin
-	second, err2 := card.ByID(2) // 2 Coin
-	third, err3 := card.ByID(3)  // 3 Coin
-	fourth, err4 := card.ByID(4) // 4 Coin
-	fifth, err5 := card.ByID(5)  // 5 Coin
-	briscola := card.Coin
-
+func verifyRoundScenario(t *testing.T, expectedWinner uint8, first, second, third, fourth, fifth *card.Card, briscola card.Seed, err1, err2, err3, err4, err5 error) {
 	if err1 != nil {
 		t.Fatal("err1 raised")
 	} else if err2 != nil {
@@ -23,10 +16,20 @@ func TestScenario1WithAceOfCoinWinning(t *testing.T) {
 		t.Fatal("err5 raised")
 	} else {
 		i := declareWinner(first, second, third, fourth, fifth, briscola)
-		if i != 0 {
-			t.Fatal("Unexpected winner")
+		if i != expectedWinner {
+			t.Fatalf("Unexpected winner: winner was %d", i)
 		}
 	}
+}
+
+func TestScenario1WithAceOfCoinWinning(t *testing.T) {
+	first, err1 := card.ByID(1)  // 1 Coin
+	second, err2 := card.ByID(2) // 2 Coin
+	third, err3 := card.ByID(3)  // 3 Coin
+	fourth, err4 := card.ByID(4) // 4 Coin
+	fifth, err5 := card.ByID(5)  // 5 Coin
+	briscola := card.Coin
+	verifyRoundScenario(t, 0, first, second, third, fourth, fifth, briscola, err1, err2, err3, err4, err5)
 }
 
 func TestScenario1WithThreeOfCoinWinning(t *testing.T) {
@@ -36,21 +39,5 @@ func TestScenario1WithThreeOfCoinWinning(t *testing.T) {
 	fourth, err4 := card.ByID(5) // 5 Coin
 	fifth, err5 := card.ByID(6)  // 6 Coin
 	briscola := card.Coin
-
-	if err1 != nil {
-		t.Fatal("err1 raised")
-	} else if err2 != nil {
-		t.Fatal("err2 raised")
-	} else if err3 != nil {
-		t.Fatal("err3 raised")
-	} else if err4 != nil {
-		t.Fatal("err4 raised")
-	} else if err5 != nil {
-		t.Fatal("err5 raised")
-	} else {
-		i := declareWinner(first, second, third, fourth, fifth, briscola)
-		if i != 1 {
-			t.Fatalf("Unexpected winner: winner was %d", i)
-		}
-	}
+	verifyRoundScenario(t, 1, first, second, third, fourth, fifth, briscola, err1, err2, err3, err4, err5)
 }
