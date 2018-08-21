0c3c885b55d8f944b59abf3b55528ea3bc944253
Michele Caci <michele.caci@amadeus.com>
2018-08-21 14:00:21 +0200

    Refactor: rename method

--- src/msdb5/card/card_err_test.go
+++ src/msdb5/card/card_err_test.go
@@ -3,13 +3,13 @@ package card
 import "testing"
 
 func TestId0IsInvalid(t *testing.T) {
-	verifyInvalidId(t, 0)
+	verifyInvalidID(t, 0)
 }
 func TestId41IsInvalid(t *testing.T) {
-	verifyInvalidId(t, 41)
+	verifyInvalidID(t, 41)
 }
 
-func verifyInvalidId(t *testing.T, id int) {
+func verifyInvalidID(t *testing.T, id int) {
 	_, err := ByID(id)
 	if err == nil {
 		t.Fatalf("%d is not valid id", id)
