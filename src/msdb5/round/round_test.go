50ef17c3be7b585256cf95117543560d40c3495e
Michele Caci <michele.caci@amadeus.com>
2018-08-21 14:23:01 +0200

    Red: add new test case for round with second card winning

--- src/msdb5/round/round_test.go
+++ src/msdb5/round/round_test.go
@@ -3,7 +3,7 @@ package round
 import "testing"
 import "msdb5/card"
 
-func TestWinnerIsAceOfCoin(t *testing.T) {
+func TestScenario1WithAceOfCoinWinning(t *testing.T) {
 	first, err1 := card.ByID(1)  // 1 Coin
 	second, err2 := card.ByID(2) // 2 Coin
 	third, err3 := card.ByID(3)  // 3 Coin
@@ -25,6 +25,32 @@ func TestWinnerIsAceOfCoin(t *testing.T) {
 		i := declareWinner(first, second, third, fourth, fifth, briscola)
 		if i != 0 {
 			t.Fatal("Unexpected winner")
+		}
+	}
+}
+
+func TestScenario1WithThreeOfCoinWinning(t *testing.T) {
+	first, err1 := card.ByID(2)  // 2 Coin
+	second, err2 := card.ByID(3) // 3 Coin
+	third, err3 := card.ByID(4)  // 4 Coin
+	fourth, err4 := card.ByID(5) // 5 Coin
+	fifth, err5 := card.ByID(6)  // 6 Coin
+	briscola := card.Coin
+
+	if err1 != nil {
+		t.Fatal("err1 raised")
+	} else if err2 != nil {
+		t.Fatal("err2 raised")
+	} else if err3 != nil {
+		t.Fatal("err3 raised")
+	} else if err4 != nil {
+		t.Fatal("err4 raised")
+	} else if err5 != nil {
+		t.Fatal("err5 raised")
+	} else {
+		i := declareWinner(first, second, third, fourth, fifth, briscola)
+		if i != 1 {
+			t.Fatalf("Unexpected winner: winner was %d", i)
 		}
 	}
 }
