54b51b119625b2c454ac6874a6027e67e8df7856
Michele Caci <michele.caci@amadeus.com>
2018-08-21 14:42:10 +0200

    Green: fix round test

--- src/msdb5/round/round.go
+++ src/msdb5/round/round.go
@@ -3,5 +3,14 @@ package round
 import "msdb5/card"
 
 func declareWinner(first, second, third, fourth, fifth *(card.Card), briscola card.Seed) uint8 {
-	return 0
+	winnerCode := -1
+	firstCode := 0
+	secondCode := 1
+
+	if first.Compare(*second) > 0 {
+		winnerCode = firstCode
+	} else {
+		winnerCode = secondCode
+	}
+	return uint8(winnerCode)
 }
