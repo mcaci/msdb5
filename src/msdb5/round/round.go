3f7d3130dc992171aa6d1374401c606be65d6bb5
Michele Caci <michele.caci@amadeus.com>
2018-08-21 14:59:52 +0200

    Refactor: cosmetic changes on method signature

--- src/msdb5/round/round.go
+++ src/msdb5/round/round.go
@@ -2,7 +2,7 @@ package round
 
 import "msdb5/card"
 
-func declareWinner(first, second, third, fourth, fifth *(card.Card), briscola card.Seed) uint8 {
+func declareWinner(first, second, third, fourth, fifth *card.Card, briscola card.Seed) uint8 {
 	winnerCode := -1
 	firstCode := 0
 	secondCode := 1
