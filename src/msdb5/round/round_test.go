1cff297efda40cb517f2a0cdd3298952db479585
Michele Caci <michele.caci@amadeus.com>
2018-08-28 13:36:34 +0200

    Red: add test with random cards for round verification

--- src/msdb5/round/round_test.go
+++ src/msdb5/round/round_test.go
@@ -4,15 +4,23 @@ import "testing"
 import "msdb5/card"
 
 func TestScenario1WithAceOfCoinWinning(t *testing.T) {
+	// testing 1 to 5 of Coin
 	verifyRoundScenario(t, cardsOnTheTable(1, 2, 3, 4, 5), card.Coin, 0)
 }
 
 func TestScenario1WithThreeOfCoinWinning(t *testing.T) {
+	// 2 to 6 of Coin
 	verifyRoundScenario(t, cardsOnTheTable(2, 3, 4, 5, 6), card.Coin, 1)
 }
 
 func TestScenario1WithEightOfCoinWinning(t *testing.T) {
+	// 4 to 8 of Coin
 	verifyRoundScenario(t, cardsOnTheTable(4, 5, 6, 7, 8), card.Coin, 4)
+}
+
+func TestScenario1WithTwoOfSwordsWinningBecauseOfBriscola(t *testing.T) {
+	// 4 to 8 of Coin
+	verifyRoundScenario(t, cardsOnTheTable(34, 40, 3, 22, 15), card.Sword, 3)
 }
 
 func cardsOnTheTable(cardIds ...uint8) [5]*card.Card {
