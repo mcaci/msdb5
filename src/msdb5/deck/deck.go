8d08a43c0f23333226dfc0fd675a7f3c1dd9d4f2
Michele Caci <michele.caci@amadeus.com>
2018-08-30 14:53:42 +0200

    Green: Add test for distributing all cards

--- src/msdb5/deck/deck.go
+++ src/msdb5/deck/deck.go
@@ -3,11 +3,13 @@ package deck
 import (
 	"math/rand"
 	"msdb5/card"
+	"time"
 )
 
 // Deck interface
 type Deck interface {
 	RemoveTop() *(card.Card)
+	IsEmpty() bool
 }
 
 // ConcreteDeck type
@@ -19,7 +21,13 @@ type ConcreteDeck struct {
 // Create func
 func (deck *ConcreteDeck) Create() {
 	const deckSize = 40
+	rand.Seed(time.Now().UnixNano())
 	deck.cards = rand.Perm(deckSize)
+}
+
+// IsEmpty func
+func (deck *ConcreteDeck) IsEmpty() bool {
+	return deck.index >= 40 // should be 40 as it's the deck's size
 }
 
 // RemoveTop func
--- src/msdb5/player/player_draws_card_test.go
+++ src/msdb5/player/player_draws_card_test.go
@@ -2,16 +2,22 @@ package player
 
 import (
 	"msdb5/card"
+	"msdb5/deck"
 	"testing"
 )
 
 type MockDeck struct {
 }
 
-func (d *MockDeck) RemoveTop() *(card.Card) {
+func (d *MockDeck) RemoveTop() *card.Card {
 	mockCard, _ := card.ByID(0)
 	return mockCard
 }
+
+func (d *MockDeck) IsEmpty() bool {
+	return false
+}
+
 func TestPlayerDrawsOneCard(t *testing.T) {
 	var d MockDeck
 	var p Player
@@ -20,5 +26,27 @@ func TestPlayerDrawsOneCard(t *testing.T) {
 	playedCard := p.Draw(&d)
 	if p.Hasnt(playedCard) {
 		t.Fatalf("Expecting player to have drawn %v", playedCard)
+	}
+}
+
+func Test5PlayersDrawUntilDeckIsEmpty(t *testing.T) { // not a Unit test
+	var deck deck.ConcreteDeck
+	deck.Create()
+
+	var players [5]Player
+	for i := range players {
+		players[i] = &ConcretePlayer{}
+		players[i].Init()
+	}
+
+	for i := 0; i < 40; i++ {
+		players[i%5].Draw(&deck)
+	}
+
+	if !deck.IsEmpty() {
+		t.Fatal("All players should have drawn all cards")
+		for _, player := range players {
+			t.Log(player)
+		}
 	}
 }
