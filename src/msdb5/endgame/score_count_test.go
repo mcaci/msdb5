f2b55d2596179922bca9ce30624af3342fc02da8
Michele Caci <michele.caci@amadeus.com>
2018-09-07 16:44:25 +0200

    Red: test to count points for two cards

--- src/msdb5/endgame/score_count_test.go
+++ src/msdb5/endgame/score_count_test.go
@@ -8,7 +8,7 @@ import (
 func testScoreCount(t *testing.T, expectedScore uint8, cards ...*card.Card) {
 	score := CountPoints(cards...)
 	if expectedScore != score {
-		t.Fatalf("Score expected is not %d but %d", expectedScore, score)
+		t.Fatalf("Score expected is %d but %d was computed", expectedScore, score)
 	}
 }
 
@@ -16,7 +16,13 @@ func TestEmptyPileSums0(t *testing.T) {
 	testScoreCount(t, 0)
 }
 
-func TestPileWitOnehAceOnlySums11(t *testing.T) {
+func TestPileWithOnehAceOnlySums11(t *testing.T) {
 	ace, _ := card.ByID(1)
 	testScoreCount(t, 11, ace)
+}
+
+func TestPileWithOneTwoOnehAceOnlySums11(t *testing.T) {
+	two, _ := card.ByID(2)
+	ace, _ := card.ByID(1)
+	testScoreCount(t, 11, two, ace)
 }
