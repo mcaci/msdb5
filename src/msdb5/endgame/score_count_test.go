7f7451ea13233b6c1ca293d4289ea24f9347531b
Michele Caci <michele.caci@amadeus.com>
2018-09-07 12:02:57 +0200

    Refactor: change slice to variadic parameter in poins count func

--- src/msdb5/endgame/score_count_test.go
+++ src/msdb5/endgame/score_count_test.go
@@ -5,11 +5,12 @@ import (
 	"testing"
 )
 
-func points(c []*card.Card) int { return 0 }
+func points(c ...*card.Card) int { return 0 }
 
-func TestEmptyPileSumsZero(t *testing.T) {
-	score := points([]*card.Card{})
+func TestEmptyPileSums11(t *testing.T) {
+	score := points()
 	if score != 0 {
 		t.Fatalf("Score expected is not 0 but %d", score)
 	}
 }
+
