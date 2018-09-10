398299d2f2ce31954df836c8f87344709f491bf2
Michele Caci <michele.caci@amadeus.com>
2018-09-10 13:17:05 +0200

    RedGreenRefactor: test for invalid id complain in card nomination

--- src/msdb5/auction/nominate_test.go
+++ src/msdb5/auction/nominate_test.go
@@ -5,15 +5,21 @@ import (
 	"msdb5/card"
 )
 
-func Nominate(id uint8) *card.Card {
-	card, _ := card.ByID(id)
-	return card
+func Nominate(id uint8) (*card.Card, error) {
+	return card.ByID(id)
+}
+
+func TestNominateId0WillComplain(t *testing.T) {
+	_, err := Nominate(0)
+	if err == nil {
+		t.Fatal("Error should be present")
+	}
 }
 
 func TestNominateId1WillNominateAceOfCoin(t *testing.T) {
-	actualCard := Nominate(1)
+	actualCard, err := Nominate(1)
 	expectedCard, _ := card.ByID(1)
-	if *expectedCard != *actualCard {
+	if err != nil || *expectedCard != *actualCard {
 		t.Fatalf("Card nominated should be %v but %v was computed", expectedCard, actualCard)
 	}
 }
