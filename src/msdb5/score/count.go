d0d06e6b11f0e572243ab36c235efcdd5fe3ea92
Michele Caci <michele.caci@amadeus.com>
2018-09-10 12:41:18 +0200

    Refactor: Compute score points renaming

--- src/msdb5/score/count.go
+++ src/msdb5/score/count.go
@@ -1,9 +1,9 @@
-package endgame
+package score
 
 import "msdb5/card"
 
-// CountPoints func
-func CountPoints(cards ...*card.Card) uint8 {
+// Compute func
+func Compute(cards ...*card.Card) uint8 {
 	var sum uint8
 	for _, card := range cards {
 		sum += card.Points()
