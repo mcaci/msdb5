93643876f4eb19bc583c72557319c70536b7542b
Michele Caci <michele.caci@amadeus.com>
2018-09-12 18:14:23 +0200

    Refactor: New method to create board introduced

--- board/board_test.go
+++ board/board_test.go
@@ -9,12 +9,16 @@ import (
 type Board struct {
 }
 
+func New() *Board {
+	return &Board{}
+}
+
 func (b *Board) Deck() deck.Deck {
 	return deck.New()
 }
 
 func TestBoardHasADeck(t *testing.T) {
-	b := Board{}
+	b := New()
 	if b.Deck() == nil {
 		t.Fatal("The board has no Deck")
 	}
