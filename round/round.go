7a1b2123aa390aaf6493f4409e07aa59adc886d3
Michele Caci <michele.caci@amadeus.com>
2018-09-27 10:33:27 +0200

    Refactor: extract function for card comparison in round

--- round/round.go
+++ round/round.go
@@ -7,12 +7,16 @@ func IndexOfWinningCard(cardsOnTheTable [5]*card.Card, briscola card.Seed) uint8
 	base := cardsOnTheTable[0]
 	max := 0
 	for i, other := range cardsOnTheTable {
-		onlyOtherCardIsBriscola := !base.IsBriscola(briscola) && other.IsBriscola(briscola)
-		otherCardIsBiggerAndOfSameSeed := card.DoesOtherCardWin(base, other)
-		if onlyOtherCardIsBriscola || otherCardIsBiggerAndOfSameSeed {
+		if doesOtherCardWin(base, other, briscola) {
 			base = other
 			max = i
 		}
 	}
 	return uint8(max)
+}
+
+func doesOtherCardWin(base, other *card.Card, briscola card.Seed) bool {
+	onlyOtherCardIsBriscola := !base.IsBriscola(briscola) && other.IsBriscola(briscola)
+	otherCardIsBiggerAndOfSameSeed := card.DoesOtherCardWin(base, other)
+	return onlyOtherCardIsBriscola || otherCardIsBiggerAndOfSameSeed
 }
