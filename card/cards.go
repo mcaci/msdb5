ff1fa2e78cebcb1149af7ce6dad05cf3d3905ea7
nikiforosFreespirit <michele.caci@gmail.com>
2018-10-15 23:05:27 +0200

    Refactor: implement methods for cards

--- card/cards.go
+++ card/cards.go
@@ -5,10 +5,17 @@ type Cards []Card
 
 // Add func
 func (cards *Cards) Add(card Card) {
-
+	*cards = append(*cards, card)
 }
 
 // Has func
 func (cards Cards) Has(card Card) bool {
-	return false
+	var cardFound bool
+	for _, c := range cards {
+		cardFound = (c == card)
+		if cardFound {
+			break
+		}
+	}
+	return cardFound
 }
