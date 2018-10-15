0180f7b9111576e5c8ce6958bc38849148a383f9
nikiforosFreespirit <michele.caci@gmail.com>
2018-10-15 23:00:48 +0200

    Refactor: introduce card set cards

---
+++ card/cards.go
@@ -0,0 +1,14 @@
+package card
+
+// Cards type
+type Cards []Card
+
+// Add func
+func (cards *Cards) Add(card Card) {
+
+}
+
+// Has func
+func (cards Cards) Has(card Card) bool {
+	return false
+}
