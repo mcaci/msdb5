be15d48fb4faf3ca596653a3dafa442817c5b904
nikiforosFreespirit <michele.caci@gmail.com>
2018-10-18 20:55:47 +0200

    Refactor: move player draw cards test to board package

--- board/board_test.go
+++ board/board_test.go
@@ -2,6 +2,9 @@ package board
 
 import (
 	"testing"
+
+	"github.com/nikiforosFreespirit/msdb5/deck"
+	"github.com/nikiforosFreespirit/msdb5/player"
 )
 
 func TestBoardHasADeck(t *testing.T) {
@@ -15,5 +18,25 @@ func TestBoardHas5Player(t *testing.T) {
 	b := New()
 	if b.Players() == nil {
 		t.Fatal("The board has no Player")
+	}
+}
+
+func Test5PlayersDrawUntilDeckIsEmpty(t *testing.T) { // not a Unit test
+	d := deck.Deck()
+
+	var players [5]player.Player
+	for i := range players {
+		players[i] = player.New()
+	}
+
+	for i := 0; i < deck.Size; i++ {
+		players[i%5].Draw(&d)
+	}
+
+	if !d.IsEmpty() {
+		t.Fatal("All players should have drawn all cards")
+		for _, player := range players {
+			t.Log(player)
+		}
 	}
 }
--- player/player_draws_card_test.go
+++ player/player_draws_card_test.go
@@ -15,23 +15,3 @@ func TestPlayerDrawsOneCard(t *testing.T) {
 		t.Fatalf("Expecting player to have drawn %v", drawnCard)
 	}
 }
-
-func Test5PlayersDrawUntilDeckIsEmpty(t *testing.T) { // not a Unit test
-	d := deck.Deck()
-
-	var players [5]Player
-	for i := range players {
-		players[i] = New()
-	}
-
-	for i := 0; i < deck.Size; i++ {
-		players[i%5].Draw(&d)
-	}
-
-	if !d.IsEmpty() {
-		t.Fatal("All players should have drawn all cards")
-		for _, player := range players {
-			t.Log(player)
-		}
-	}
-}
