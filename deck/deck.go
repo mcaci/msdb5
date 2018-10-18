6dfe34bbc6e8e3e0a2f9179cdafb2d0cf5f2168c
nikiforosFreespirit <michele.caci@gmail.com>
2018-10-18 20:44:20 +0200

    Refactor: rename receivers to get closer to cardCards

--- deck/deck.go
+++ deck/deck.go
@@ -11,41 +11,41 @@ import (
 type Cards []uint8
 
 // GetIDs func
-func (deck *Cards) GetIDs() []uint8 {
-	return []uint8(*deck)
+func (cards *Cards) GetIDs() []uint8 {
+	return []uint8(*cards)
 }
 
 // IsEmpty func
-func (deck *Cards) IsEmpty() bool {
-	return len(*deck) <= 0 // should be 40 as it's the deck's size
+func (cards *Cards) IsEmpty() bool {
+	return len(*cards) <= 0 // should be 40 as it's the cards's size
 }
 
 // Supply func
-func (deck *Cards) Supply() card.Card {
-	card, err := card.ByID((*deck)[0])
+func (cards *Cards) Supply() card.Card {
+	card, err := card.ByID((*cards)[0])
 	if err != nil {
 		panic("Should not be here: " + err.Error())
 	} else {
-		deck.updateDeck()
+		cards.updateDeck()
 	}
 	return card
 }
 
-func (deck *Cards) updateDeck() {
-	(*deck) = (*deck)[1:]
+func (cards *Cards) updateDeck() {
+	(*cards) = (*cards)[1:]
 }
 
-// Size of a deck of cards
+// Size of a cards of cards
 const Size = 40
 
 // Deck func
 func Deck() Cards {
-	deck := new(Cards)
+	cards := new(Cards)
 
 	rand.Seed(time.Now().UnixNano())
 	ints := rand.Perm(Size)
 	for index := range ints {
-		*deck = append(*deck, uint8(ints[index]+1))
+		*cards = append(*cards, uint8(ints[index]+1))
 	}
-	return *deck
+	return *cards
 }
