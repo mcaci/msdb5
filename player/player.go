2b4a12c7ce01816bb03c2547d98ee0d186686f24
Michele Caci <michele.caci@amadeus.com>
2018-10-22 13:28:01 +0200

    Refactor: remove concrete player file

--- player/player.go
+++ player/player.go
@@ -1,27 +1,63 @@
 package player
 
 import (
-	"fmt"
-
 	"github.com/nikiforosFreespirit/msdb5/card"
 )
 
-// Player interface
-type Player interface {
-	Draw(card.Supplier) card.ID
-	Name() string
-	Hand() *card.Cards
-	fmt.Stringer
-
-	Iam(string)
-	MyHostIs(string)
-
-	Has(card.ID) bool
+// Player struct
+type Player struct {
+	name string
+	host string
+	hand card.Cards
 }
 
 // New func
 func New() Player {
-	player := new(concretePlayer)
+	player := new(Player)
 	player.hand = card.Cards{}
-	return player
+	return *player
+}
+
+// Draw func
+func (player *Player) Draw(cardSupplier card.Supplier) card.ID {
+	c := cardSupplier.Supply()
+	player.Hand().Add(c)
+	return c
+}
+
+// Hand func
+func (player *Player) Hand() *card.Cards {
+	return &player.hand
+}
+
+// Name func
+func (player *Player) Name() string {
+	return player.name
+}
+
+// Iam func
+func (player *Player) Iam(name string) {
+	player.name = name
+}
+
+// MyHostIs func
+func (player *Player) MyHostIs(host string) {
+	player.host = host
+}
+
+// Has func
+func (player *Player) Has(id card.ID) bool {
+	return player.Hand().Has(id)
+}
+
+func (player Player) String() string {
+	str := "Player["
+	str += "Name:" + player.name + ";"
+	str += "Host:" + player.host + ";"
+	for _, cardID := range player.hand {
+		c, _ := card.ByID(cardID)
+		str += c.String() + " "
+	}
+	str += "]"
+	return str
 }
