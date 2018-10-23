70c963237601b7ab51ad859cd1e00e3e429c1519
Michele Caci <michele.caci@amadeus.com>
2018-10-23 12:55:46 +0200

    Red: test search player by host when name is not provided

--- player/player_byname_test.go
+++ player/player_byname_test.go
@@ -11,3 +11,11 @@ func TestPlayerPresentInList(t *testing.T) {
 		t.Fatalf("%v and %v are expected to be the same player", player, playerByName)
 	}
 }
+
+func TestPlayerPresentInListByHost(t *testing.T) {
+	player := Player{host: "A"}
+	playerByName := ByName("A", []*Player{&Player{host: "A"}, &Player{host: "B"}})
+	if player.Name() != playerByName.Name() {
+		t.Fatalf("%v and %v are expected to be the same player", player, playerByName)
+	}
+}
