ff6dc46ffa83ca4ca10b227cdc068f0bc316efc2
Michele Caci <michele.caci@amadeus.com>
2018-10-24 13:48:03 +0200

    Green: test no error for found and error for not found player

--- player/player_byname_test.go
+++ player/player_byname_test.go
@@ -13,19 +13,24 @@ func TestPlayerPresentInList(t *testing.T) {
 }
 
 func TestPlayerPresentInListNoErr(t *testing.T) {
-	_, err := ByName("A", []*Player{&Player{name: "A"}, &Player{name: "B"}})
-	if err != nil {
+	if _, err := ByName("A", []*Player{&Player{name: "A"}, &Player{name: "B"}}); err != nil {
 		t.Fatal(err)
 	}
 }
 
-func TestPlayerPresentInListByHost(t *testing.T) {
-	// player := Player{host: "A"}
-	_, err := ByName("A", []*Player{&Player{host: "A"}, &Player{host: "B"}})
-	if err != nil {
+func TestPlayerNotPresentToReturnErr(t *testing.T) {
+	if _, err := ByName("C", []*Player{&Player{name: "A"}, &Player{name: "B"}}); err == nil {
 		t.Fatal(err)
 	}
-	// if player.Name() != playerByName.Name() {
-	// 	t.Fatalf("%v and %v are expected to be the same player", player, playerByName)
-	// }
 }
+
+// func TestPlayerPresentInListByHost(t *testing.T) {
+// 	// player := Player{host: "A"}
+// 	_, err := ByName("A", []*Player{&Player{host: "A"}, &Player{host: "B"}})
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	// if player.Name() != playerByName.Name() {
+// 	// 	t.Fatalf("%v and %v are expected to be the same player", player, playerByName)
+// 	// }
+// }
