c75b93565ae4da202ed28fa65231e39b8b5c66fb
Michele Caci <michele.caci@amadeus.com>
2018-10-24 13:56:56 +0200

    Red: take test to find by hostname again

--- player/player_byname_test.go
+++ player/player_byname_test.go
@@ -26,13 +26,13 @@ func TestPlayerNotPresentToReturnErr(t *testing.T) {
 	errorCheck(t, "C", func(e error) bool { return e == nil })
 }
 
-// func TestPlayerPresentInListByHost(t *testing.T) {
-// 	// player := Player{host: "A"}
-// 	_, err := ByName("A", []*Player{&Player{host: "A"}, &Player{host: "B"}})
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	// if player.Name() != player.Name() {
-// 	// 	t.Fatalf("%v and %v are expected to be the same player", player, player)
-// 	// }
-// }
+func TestPlayerPresentInListByHost(t *testing.T) {
+	// player := Player{host: "A"}
+	_, err := ByName("A", []*Player{&Player{host: "A"}, &Player{host: "B"}})
+	if err != nil {
+		t.Fatal(err)
+	}
+	// if player.Name() != player.Name() {
+	// 	t.Fatalf("%v and %v are expected to be the same player", player, player)
+	// }
+}
