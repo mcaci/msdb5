8ce9f2da3d23ccd89f2b7196e46e7ff81e394b24
Michele Caci <michele.caci@amadeus.com>
2018-10-24 16:07:48 +0200

    Refactor: remove unused code in player find

--- player/player_find.go
+++ player/player_find.go
@@ -4,21 +4,19 @@ import "errors"
 
 // Find func
 func Find(nameOrHost string, players []*Player) (p *Player, err error) {
-	p, found := findInfoIn(players, func(pl *Player) bool { return pl.Name() == nameOrHost })
-	if !found {
-		p, found = findInfoIn(players, func(pl *Player) bool { return pl.Host() == nameOrHost })
-	}
+	findCriteria := func(p *Player) bool { return p.Name() == nameOrHost || p.Host() == nameOrHost }
+	p, found := findInfoIn(players, findCriteria)
 	if !found {
 		err = errors.New("Player " + nameOrHost + " not found")
 	}
 	return
 }
 
-func findInfoIn(players []*Player, isInfoPresent func(*Player) bool) (player *Player, found bool) {
+func findInfoIn(players []*Player, isInfoPresent func(*Player) bool) (*Player, bool) {
 	for _, p := range players {
 		if isInfoPresent(p) {
 			return p, true
 		}
 	}
-	return
+	return nil, false
 }
