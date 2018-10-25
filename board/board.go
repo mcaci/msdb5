2433c71a825f351b0bf0d61bdf2421aa5e6638ff
nikiforosFreespirit <michele.caci@gmail.com>
2018-10-25 23:13:31 +0200

    Refactor: extract and simplify new board func

--- board/board.go
+++ board/board.go
@@ -14,21 +14,23 @@ type Board struct {
 
 // New func
 func New() *Board {
-	var b Board
+	b := new(Board)
+	makePlayers(b)
+	playersDrawAllCards(&b.players)
+	return b
+}
 
-	b.players = make([]*player.Player, 5)
-	for i := range b.players {
-		b.players[i] = player.New()
+func makePlayers(b *Board) {
+	for i := 0; i < 5; i++ {
+		b.players = append(b.players, player.New())
 	}
+}
 
+func playersDrawAllCards(players *[]*player.Player) {
 	deck := card.Deck()
 	for i := 0; i < card.DeckSize; i++ {
-		b.players[i%5].Draw(&deck)
+		(*players)[i%5].Draw(&deck)
 	}
-
-	b.playedCards = card.Cards{}
-
-	return &b
 }
 
 // Players func
