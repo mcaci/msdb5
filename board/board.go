ce9723211e1fc00dccd2ac3fe799be9dad6cda1b
nikiforosFreespirit <michele.caci@gmail.com>
2018-10-25 23:05:23 +0200

    Refactor: Remove references to board deck

--- board/board.go
+++ board/board.go
@@ -7,7 +7,6 @@ import (
 
 // Board struct
 type Board struct {
-	deck         card.Cards
 	players      []*player.Player
 	playedCards  card.Cards
 	auctionScore uint8
@@ -17,24 +16,19 @@ type Board struct {
 func New() *Board {
 	var b Board
 
-	b.deck = card.Deck()
-
 	b.players = make([]*player.Player, 5)
 	for i := range b.players {
 		b.players[i] = player.New()
 	}
+
+	deck := card.Deck()
 	for i := 0; i < card.DeckSize; i++ {
-		b.players[i%5].Draw(&b.deck)
+		b.players[i%5].Draw(&deck)
 	}
 
 	b.playedCards = card.Cards{}
 
 	return &b
-}
-
-// Deck func
-func (b *Board) Deck() card.Cards {
-	return b.deck
 }
 
 // Players func
--- board/board_test.go
+++ board/board_test.go
@@ -4,21 +4,9 @@ import (
 	"testing"
 )
 
-func TestBoardHasADeck(t *testing.T) {
-	if b := New(); b.Deck() == nil {
-		t.Fatal("The board has no Deck")
-	}
-}
-
 func TestBoardHas5Player(t *testing.T) {
 	if b := New(); b.Players() == nil {
 		t.Fatal("The board has no Player")
-	}
-}
-
-func Test5PlayersDrawUntilDeckIsEmpty(t *testing.T) {
-	if b := New(); len(b.Deck()) > 0 {
-		t.Fatal("Not all cards have been distributed")
 	}
 }
 
