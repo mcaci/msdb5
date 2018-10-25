a0f529e645a33cefd932e98c26e05efe9af67490
nikiforosFreespirit <michele.caci@gmail.com>
2018-10-25 23:03:07 +0200

    Green: update score from api

--- board/board.go
+++ board/board.go
@@ -47,6 +47,11 @@ func (b *Board) PlayedCards() *card.Cards {
 	return &b.playedCards
 }
 
+// SetAuctionScore func
+func (b *Board) SetAuctionScore(score uint8) {
+	b.auctionScore = score
+}
+
 // AuctionScore func
 func (b *Board) AuctionScore() uint8 {
 	return b.auctionScore
