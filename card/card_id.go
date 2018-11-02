857fbb2d91458f46f36cd256a69b87ff53b8e264
Michele Caci <michele.caci@amadeus.com>
2018-11-02 17:13:13 +0100

    Refactor: bring back ID generator to card file

--- card/card_id.go
+++ card/card_id.go
@@ -3,11 +3,6 @@ package card
 // ID is the id of a card from 1 to 40
 type ID uint8
 
-// ID func
-func (card *Card) ID() ID {
-	return ID(card.number + (uint8)(card.seed)*10)
-}
-
 func toZeroBased(id ID) uint8 {
 	return uint8(id) - 1
 }
