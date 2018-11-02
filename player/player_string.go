a16c71abaa150a19c65020592830721147314dbc
Michele Caci <michele.caci@amadeus.com>
2018-11-02 13:51:14 +0100

    Refactor: stringer for player updated

--- player/player_string.go
+++ player/player_string.go
@@ -1,23 +1,11 @@
 package player
 
-import (
-	"github.com/nikiforosFreespirit/msdb5/card"
-)
-
 func (player Player) String() string {
 	str := "Player["
 	str += "Name:" + player.name + ";"
 	str += "Host:" + player.host + ";"
-	str += "Hand:"
-	for _, cardID := range player.hand {
-		c, _ := card.ByID(cardID)
-		str += c.String() + " "
-	}
-	str += "Pile:"
-	for _, cardID := range player.pile {
-		c, _ := card.ByID(cardID)
-		str += c.String() + " "
-	}
+	str += "Hand:" + player.hand.String() + ";"
+	str += "Pile:" + player.pile.String() + ";"
 	str += "]"
 	return str
 }
