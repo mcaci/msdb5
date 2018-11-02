4b99e85229893842de0eeea37216ea057d0b3f06
Michele Caci <michele.caci@amadeus.com>
2018-11-02 16:41:26 +0100

    Refactor: Simplify conversion from id to number and seed

--- card/card_byid.go
+++ card/card_byid.go
@@ -16,18 +16,17 @@ func ByID(id ID) (Card, error) {
 	} else if id > 40 {
 		err = errors.New("Index cannot be more than 40")
 	} else {
-		seedIndex := uint8(id-1) / 10
-		number := intoNumber(id, seedIndex)
-		seed := intoSeed(seedIndex)
+		number := intoNumber(id)
+		seed := intoSeed(id)
 		card = Card{number: number, seed: seed}
 	}
 	return card, err
 }
 
-func intoNumber(id ID, seedIndex uint8) uint8 {
-	return uint8(id) - (10 * seedIndex)
+func intoNumber(id ID) uint8 {
+	return (uint8(id)-1)%10 + 1
 }
 
-func intoSeed(seedIndex uint8) Seed {
-	return Seed(seedIndex)
+func intoSeed(id ID) Seed {
+	return Seed((uint8(id) - 1) / 10)
 }
