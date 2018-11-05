8a93be115cad57dc755eaac8ee608e75411c2b4b
Michele Caci <michele.caci@amadeus.com>
2018-11-05 15:26:22 +0100

    Refactor: Align cardbyId to make it similar to cardByName

--- card/card_byid.go
+++ card/card_byid.go
@@ -6,22 +6,25 @@ import (
 
 // ByID func
 func ByID(id ID) (Card, error) {
-	var card Card
+	var c Card
+	var err error
+	if c.number, err = extractNumber(id); err == nil {
+		c.seed, _ = extractSeed(id)
+	}
+	return c, err
+}
+
+func extractNumber(id ID) (uint8, error) {
+	var n uint8
 	var err error
 	if id < 1 {
 		err = errors.New("Index cannot be less than 1")
 	} else if id > 40 {
 		err = errors.New("Index cannot be more than 40")
 	} else {
-		number, _ := extractNumber(id)
-		seed, _ := extractSeed(id)
-		card = Card{number: number, seed: seed}
+		n = toZeroBased(id)%10 + 1
 	}
-	return card, err
-}
-
-func extractNumber(id ID) (uint8, error) {
-	return toZeroBased(id)%10 + 1, nil
+	return n, err
 }
 
 func extractSeed(id ID) (Seed, error) {
