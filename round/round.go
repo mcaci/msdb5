e37eda4083ed81945b90aec16dbff2e05640bab4
Michele Caci <michele.caci@amadeus.com>
2018-11-06 13:56:18 +0100

    Refactor: round package uses card id

--- round/round.go
+++ round/round.go
@@ -7,13 +7,11 @@ import (
 
 // IndexOfWinningCard func
 func IndexOfWinningCard(cardsOnTheTable card.Cards, briscola card.Seed) uint8 {
-	baseID := cardsOnTheTable[0]
+	base := cardsOnTheTable[0]
 	max := 0
-	for i, otherID := range cardsOnTheTable {
-		base, _ := card.By(baseID)
-		other, _ := card.By(otherID)
+	for i, other := range cardsOnTheTable {
 		if rule.DoesOtherCardWin(base, other, briscola) {
-			baseID = otherID
+			base = other
 			max = i
 		}
 	}
