f3fcc0405745a9c2dd0a328d3c746ce4185d70b9
Michele Caci <michele.caci@amadeus.com>
2018-11-06 12:50:46 +0100

    Refactor: aggregate all card structure code

--- card/card.go
+++ card/card.go
@@ -6,6 +6,41 @@ type Card struct {
 	seed   Seed
 }
 
+// HCard is a card represented by string
+type HCard struct {
+	number, seed string
+}
+
+// ID is the id of a card from 1 to 40
+type ID uint8
+
+// Creator interface to represent what's needed to create a card
+type Creator interface {
+	toNumber() (uint8, error)
+	toSeed() (Seed, error)
+}
+
+// By func
+func By(hc Creator) (Card, error) {
+	var c Card
+	var err error
+	if c.number, err = hc.toNumber(); err == nil {
+		c.seed, err = hc.toSeed()
+	}
+	return c, err
+}
+
+// ByName func
+func ByName(number, seed string) (Card, error) {
+	hc := HCard{number, seed}
+	var c Card
+	var err error
+	if c.number, err = hc.toNumber(); err == nil {
+		c.seed, err = hc.toSeed()
+	}
+	return c, err
+}
+
 // ID func
 func (card *Card) ID() ID {
 	return ID(card.number + (uint8)(card.seed)*10)
