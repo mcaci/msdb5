f2600cb8fcb7a092d7293c71d006fdff7a02a5d4
nikiforosFreespirit <michele.caci@gmail.com>
2018-11-08 19:28:15 +0100

    Green: mode tests for rule package

--- rule/briscola_test.go
+++ rule/briscola_test.go
@@ -1,53 +1,30 @@
 package rule
 
-import (
-	"testing"
+import "testing"
+import "github.com/nikiforosFreespirit/msdb5/card"
 
-	"github.com/nikiforosFreespirit/msdb5/card"
-)
-
-var firstCardWins = func(b bool) bool { return !b }
-var firstCardLoses = func(b bool) bool { return b }
-
-func TestInClassicNumericalComparisonWithSameSeedHigherNumberWins(t *testing.T) {
-	a, _ := card.ByName("2", "Coin")
-	b, _ := card.ByName("4", "Coin")
-	verifyCardComparison(t, a, b, firstCardLoses)
+func TestScenarioWithAceOfCoinWinning(t *testing.T) {
+	// testing 1 and 2 of Coin, briscola is Coin
+	verifyRoundScenario(t, 1, 2, card.Coin, false)
 }
 
-func TestComparisonWithSameSeedThat3isHigherThan10(t *testing.T) {
-	a, _ := card.ByName("10", "Coin")
-	b, _ := card.ByName("3", "Coin")
-	verifyCardComparison(t, a, b, firstCardLoses)
+func TestScenarioWithThreeOfCoinWinning(t *testing.T) {
+	// 2 and 3 of Coin, briscola is Coin
+	verifyRoundScenario(t, 2, 3, card.Coin, true)
 }
 
-func TestComparisonWithSameSeedThat1isHigherThan9(t *testing.T) {
-	a, _ := card.ByName("1", "Coin")
-	b, _ := card.ByName("9", "Coin")
-	verifyCardComparison(t, a, b, firstCardWins)
+func TestScenarioWithEightOfCoinWinning(t *testing.T) {
+	// 10 and 4 of Coin, briscola is Cup
+	verifyRoundScenario(t, 10, 4, card.Cup, false)
 }
 
-func TestComparisonWithSameSeedThat8isHigherThan7(t *testing.T) {
-	a, _ := card.ByName("8", "Coin")
-	b, _ := card.ByName("7", "Coin")
-	verifyCardComparison(t, a, b, firstCardWins)
+func TestScenarioWithTwoOfSwordsWinningBecauseOfBriscola(t *testing.T) {
+	// 3 of Coin and 2 of Sword, briscola is Sword
+	verifyRoundScenario(t, 3, 22, card.Sword, true)
 }
 
-func TestComparisonWithSecondCardOfDifferentSeedThatFirstCardAlwaysWins(t *testing.T) {
-	a, _ := card.ByName("2", "Sword")
-	b, _ := card.ByName("3", "Cup")
-	verifyCardComparison(t, a, b, firstCardWins)
-}
-
-func TestComparisonWithEmptyCardThatNonEmptyCardWins(t *testing.T) {
-	a, _ := card.ByName("8", "Coin")
-	var b card.ID
-	verifyCardComparison(t, a, b, firstCardWins)
-}
-
-func verifyCardComparison(t *testing.T, a, b card.ID, isOtherCardExpectedToWin func(bool) bool) {
-	result := doesOtherCardWin(a, b)
-	if !isOtherCardExpectedToWin(result) {
-		t.Fatalf("Expected %v to be higher than %v", b, a)
+func verifyRoundScenario(t *testing.T, a, b card.ID, briscola card.Seed, expectedWinner bool) {
+	if index := DoesOtherCardWin(a, b, briscola); index != expectedWinner {
+		t.Fatal("Unexpected winner")
 	}
 }
