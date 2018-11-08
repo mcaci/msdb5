a7de25b566439804aeaad862117bad61fc3774aa
nikiforosFreespirit <michele.caci@gmail.com>
2018-11-08 18:51:25 +0100

    Green: add test for setting auction score

--- board/board_auction_score_test.go
+++ board/board_auction_score_test.go
@@ -9,3 +9,12 @@ func TestBoardAuctionScoreAtCreationIs0(t *testing.T) {
 		t.Fatalf("Auction score for a new board should be 0 but is %d", b.AuctionScore())
 	}
 }
+
+
+func TestBoardAuctionScoreCanBeSet(t *testing.T) {
+	b := New()
+	b.SetAuctionScore(80)
+	if b.AuctionScore() != 80 {
+		t.Fatalf("Auction score for a new board should be 80 but is %d", b.AuctionScore())
+	}
+}
