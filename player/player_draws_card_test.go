8532d54c340918cd88f697304521d8fe94b3caf6
Michele Caci <michele.caci@amadeus.com>
2018-11-13 13:30:05 +0100

    Refactor: Use dependency injection on Draw card test

--- player/player_draws_card_test.go
+++ player/player_draws_card_test.go
@@ -6,11 +6,13 @@ import (
 	"github.com/nikiforosFreespirit/msdb5/card"
 )
 
-func TestPlayerDrawsOneCard(t *testing.T) {
-	d := card.Deck()
+type MockDeck struct{}
 
+func (d *MockDeck) Supply() card.ID { return 1 }
+
+func TestPlayerDrawsOneCard(t *testing.T) {
 	player := New()
-	drawnCard := player.Draw(&d)
+	drawnCard := player.Draw(&MockDeck{})
 	if !player.Has(drawnCard) {
 		t.Fatalf("Expecting player to have drawn %v", drawnCard)
 	}
