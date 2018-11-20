a3ed6d8124c46c789e2bed26389c30994744d10c
nikiforosFreespirit <michele.caci@gmail.com>
2018-11-20 21:18:54 +0100

    Refactor: remove duplicates on channel test

--- board/board_auction_channel_test.go
+++ board/board_auction_channel_test.go
@@ -5,43 +5,41 @@ import (
 )
 
 func TestAuctionSentByChannel(t *testing.T) {
-	// set up
-	b := New()
-	b.SetAuctionScore(60)
-	cToPlayer := make(chan uint8, 1)
-	cFromPlayer := make(chan uint8, 1)
+	b, cToPlayer, cFromPlayer := setUp()
 
-	// send score to player
-	cToPlayer <- b.AuctionScore()
+	b.SetAuctionScore(tellAuction(b.AuctionScore(), cToPlayer, cFromPlayer))
 
-	// player sends his score
-	go playerTellsScore(cToPlayer, cFromPlayer)
-
-	// board receives player's score
-	b.SetAuctionScore(<-cFromPlayer)
-
-	// verify score increase
-	if b.AuctionScore() != 61 {
-		t.Fatal("score is not the 61")
-	}
+	verification(t, 61, b.AuctionScore())
 }
 
 func TestAuctionSentByChannelTwice(t *testing.T) {
-	// set up
+	b, cToPlayer, cFromPlayer := setUp()
+
+	b.SetAuctionScore(tellAuction(b.AuctionScore(), cToPlayer, cFromPlayer))
+	b.SetAuctionScore(tellAuction(b.AuctionScore(), cToPlayer, cFromPlayer))
+
+	verification(t, 62, b.AuctionScore())
+}
+
+func TestAuctionSentByChannelThrice(t *testing.T) {
+	b, cToPlayer, cFromPlayer := setUp()
+
+	b.SetAuctionScore(tellAuction(b.AuctionScore(), cToPlayer, cFromPlayer))
+	b.SetAuctionScore(tellAuction(b.AuctionScore(), cToPlayer, cFromPlayer))
+	b.SetAuctionScore(tellAuction(b.AuctionScore(), cToPlayer, cFromPlayer))
+
+	verification(t, 63, b.AuctionScore())
+}
+
+func setUp() (*Board, chan uint8, chan uint8) {
 	b := New()
 	b.SetAuctionScore(60)
 	cToPlayer := make(chan uint8, 1)
 	cFromPlayer := make(chan uint8, 1)
-
-	b.SetAuctionScore(round(b.AuctionScore(), cToPlayer, cFromPlayer))
-	b.SetAuctionScore(round(b.AuctionScore(), cToPlayer, cFromPlayer))
-
-	// verify score increase
-	if b.AuctionScore() != 62 {
-		t.Fatalf("score is not the 62 but %d", b.AuctionScore())
-	}
+	return b, cToPlayer, cFromPlayer
 }
-func round(previousScore uint8, cToPlayer chan uint8, cFromPlayer chan uint8) uint8 {
+
+func tellAuction(previousScore uint8, cToPlayer chan uint8, cFromPlayer chan uint8) uint8 {
 	// send score to player
 	cToPlayer <- previousScore
 	// player sends his score
@@ -53,4 +51,11 @@ func round(previousScore uint8, cToPlayer chan uint8, cFromPlayer chan uint8) ui
 func playerTellsScore(cToPlayer <-chan uint8, cFromPlayer chan<- uint8) {
 	actualScore := <-cToPlayer
 	cFromPlayer <- actualScore + 1
+}
+
+func verification(t *testing.T, expected, actual uint8) {
+	// verify score increase
+	if actual != expected {
+		t.Fatalf("score is not the %d but %d", expected, actual)
+	}
 }
