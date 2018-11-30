466818192be915f27376c9dd2331a073da852700
Michele Caci <michele.caci@amadeus.com>
2018-11-30 15:41:20 +0100

    Refactor: remove obivous dupes in player score test - end flocking rule

--- player/player_score_test.go
+++ player/player_score_test.go
@@ -3,37 +3,27 @@ package player
 import (
 	"testing"
 
-	"github.com/nikiforosFreespirit/msdb5/rule"
-
 	"github.com/nikiforosFreespirit/msdb5/card/set"
+	"github.com/nikiforosFreespirit/msdb5/rule"
 )
 
-func TestPlayerHasScoreOf0WhenCreated(t *testing.T) {
+func setupAndVerify(t *testing.T, cards set.Cards, expectedScore uint8) {
 	p := New()
-	p.Collect(set.Cards{})
-	expected := uint8(0)
-	actual := p.Score(rule.Count)
-	if expected != actual {
-		t.Fatalf("Score should be %d but is %d", expected, actual)
+	p.Collect(cards)
+	actualScore := p.Score(rule.Count)
+	if expectedScore != actualScore {
+		t.Fatalf("Score should be %d but is %d", expectedScore, actualScore)
 	}
+}
+
+func TestPlayerHasScoreOf0WhenCreated(t *testing.T) {
+	setupAndVerify(t, set.Cards{}, 0)
 }
 
 func TestPlayerHasScoreOf2WhenCollectingCard8AndOtherCardsScoring0(t *testing.T) {
-	p := New()
-	p.Collect(set.Cards{8, 32, 16, 4, 35})
-	expected := uint8(2)
-	actual := p.Score(rule.Count)
-	if expected != actual {
-		t.Fatalf("Score should be %d but is %d", expected, actual)
-	}
+	setupAndVerify(t, set.Cards{8, 32, 16, 4, 35}, 2)
 }
 
 func TestPlayerHasScoreOf120WhenCollectingAllCards(t *testing.T) {
-	p := New()
-	p.Collect(set.Deck())
-	expected := uint8(120)
-	actual := p.Score(rule.Count)
-	if expected != actual {
-		t.Fatalf("Score should be %d but is %d", expected, actual)
-	}
+	setupAndVerify(t, set.Deck(), 120)
 }
