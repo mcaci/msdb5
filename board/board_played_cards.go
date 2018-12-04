69adafdd5199667a2333cd995404cb054e1114b2
Michele Caci <michele.caci@amadeus.com>
2018-12-04 18:42:11 +0100

    Green: clean imports

--- board/board_played_cards.go
+++ board/board_played_cards.go
@@ -1,9 +1,7 @@
 package board
 
 import (
-	"github.com/nikiforosFreespirit/msdb5/card"
 	"github.com/nikiforosFreespirit/msdb5/card/set"
-	"github.com/nikiforosFreespirit/msdb5/rule"
 )
 
 // PlayedCards func
