53a36612fec65ab1b77185ad319afcf3b6b22f83
Michele Caci <michele.caci@amadeus.com>
2018-12-05 18:15:20 +0100

    Refactor: move code closer for rule flocking rule

--- rule/briscola.go
+++ rule/briscola.go
@@ -2,7 +2,21 @@ package rule
 
 import (
 	"github.com/nikiforosFreespirit/msdb5/card"
+	"github.com/nikiforosFreespirit/msdb5/card/set"
 )
+
+// IndexOfWinningCard func
+func IndexOfWinningCard(cardsOnTheTable set.Cards, briscola card.Seed) uint8 {
+	base := cardsOnTheTable[0]
+	max := 0
+	for i, other := range cardsOnTheTable {
+		if DoesOtherCardWin(base, other, briscola) {
+			base = other
+			max = i
+		}
+	}
+	return uint8(max)
+}
 
 // WinningCard func
 func WinningCard(base, other card.ID, briscola card.Seed) card.ID {
