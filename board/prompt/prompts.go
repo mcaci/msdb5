226713b9de73954b726ef7b3aa500d60dccd8b72
Michele Caci <michele.caci@amadeus.com>
2018-12-07 16:39:54 +0100

    Green: Add implementation for prompt score

--- board/prompt/prompts.go
+++ board/prompt/prompts.go
@@ -9,3 +9,9 @@ func PromptCard(prompt func(chan<- card.ID), cardChan chan card.ID) card.ID {
 	go prompt(cardChan)
 	return <-cardChan
 }
+
+// PromptScore func
+func PromptScore(prompt func(chan<- uint8), cardChan chan uint8) uint8 {
+	go prompt(cardChan)
+	return <-cardChan
+}
