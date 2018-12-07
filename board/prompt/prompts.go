5c0685aad9b0dd7a05974ae3caada3aa5769bf50
Michele Caci <michele.caci@amadeus.com>
2018-12-07 16:45:12 +0100

    Green: simple implementation for evaluate score

--- board/prompt/prompts.go
+++ board/prompt/prompts.go
@@ -1,6 +1,8 @@
 package prompt
 
 import (
+	"errors"
+
 	"github.com/nikiforosFreespirit/msdb5/card"
 )
 
@@ -14,4 +16,8 @@ func PromptCard(prompt func(chan<- card.ID), cardChan chan card.ID) card.ID {
 func PromptScore(prompt func(chan<- uint8), cardChan chan uint8) uint8 {
 	go prompt(cardChan)
 	return <-cardChan
+}
+
+func EvaluateScore(before, after uint8) error {
+	return errors.New("Score is low")
 }
