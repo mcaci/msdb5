587c24aeeb614cecadfd1b3f2150ad954bb99230
Michele Caci <michele.caci@amadeus.com>
2018-12-10 13:28:01 +0100

    Red: board prompts card test

--- board/nominate_test.go
+++ board/nominate_test.go
@@ -2,22 +2,13 @@ package board
 
 import (
 	"testing"
-
-	"github.com/nikiforosFreespirit/msdb5/card"
 )
-
-func TestNominateId0WillComplain(t *testing.T) {
-	board := New()
-	if _, err := board.Nominate("0", "Coin"); err == nil {
-		t.Fatal("Error should be present")
-	}
-}
 
 func TestNominateId1WillNominateAceOfCoin(t *testing.T) {
 	board := New()
-	actualCard, err := board.Nominate("1", "Coin")
-	expectedCard, _ := card.ByName("1", "Coin")
-	if err != nil || expectedCard != actualCard {
+	actualCard := board.AskNominatedCard()
+	expectedCard := board.NominatedCard()
+	if expectedCard != actualCard {
 		t.Fatalf("Card nominated should be %v but %v was computed", expectedCard, actualCard)
 	}
 }
