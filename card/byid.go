c2a54a56e63c6180f0d92e8d72527c2de3eba0d4
Michele Caci <michele.caci@amadeus.com>
2018-12-10 13:31:09 +0100

    Refactor: remove file card go

--- card/byid.go
+++ card/byid.go
@@ -1,5 +1,32 @@
 package card
 
+import "errors"
+
+// ID is the id of a card from 1 to 40
+type ID uint8
+
+// Card func
+func Card(index uint8) (id ID, err error) {
+	if index < 1 {
+		err = errors.New("Index cannot be less than 1")
+	} else if index > 40 {
+		err = errors.New("Index cannot be more than 40")
+	} else {
+		id = ID(index)
+	}
+	return
+}
+
+// Number func
+func (card *ID) Number() uint8 {
+	return card.ToNumber()
+}
+
+// Seed func
+func (card *ID) Seed() Seed {
+	return card.ToSeed()
+}
+
 // ToNumber func
 func (id ID) ToNumber() uint8 {
 	return id.toZeroBased()%10 + 1
