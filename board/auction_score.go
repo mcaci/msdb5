0f6526fbb8d76e9ce40e43d6d67d47e16ad23d62
Michele Caci <michele.caci@amadeus.com>
2019-01-16 13:02:52 +0100

    Refactor: remove magic numbers from auction code

--- board/auction_score.go
+++ board/auction_score.go
@@ -2,18 +2,21 @@ package board
 
 import "strconv"
 
+const minScore = 61
+const maxScore = 120
+
 // RaiseAuction func
 func (b *Board) RaiseAuction(score string) {
 	prevScore := int(b.AuctionScore())
 	intScore, _ := strconv.Atoi(score)
-	if intScore < 61 {
-		intScore = 61
+	if intScore < minScore {
+		intScore = minScore
 	}
 	if intScore <= prevScore {
 		intScore = prevScore
 	}
-	if intScore > 120 {
-		intScore = 120
+	if intScore > maxScore {
+		intScore = maxScore
 	}
 	b.SetAuctionScore(uint8(intScore))
 }
