cbcaf3e139de70b5196b032573b00de225779bf3
Michele Caci <michele.caci@amadeus.com>
2019-02-01 13:41:29 +0100

    Red: remove card from set test

--- card/cards_test.go
+++ card/cards_test.go
@@ -8,3 +8,11 @@ func TestCreateSet(t *testing.T) {
 		t.Fatalf("There should be the 5 of Cup card in the set")
 	}
 }
+
+func TestRemoveCardFromSet(t *testing.T) {
+	cards := Cards{15}
+	cards.Remove(0)
+	if cards.Has(15) {
+		t.Fatalf("Deck should be empty")
+	}
+}
