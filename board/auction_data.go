54084d9a2a386e4fac543063b3dc632539c64f70
Michele Caci <michele.caci@amadeus.com>
2019-02-05 14:08:46 +0100

    Refactor: move unexported method below

--- board/auction_data.go
+++ board/auction_data.go
@@ -16,13 +16,6 @@ func NewAuctionWithReturnScore(score, ret int, compare func(int, int) bool) *Auc
 	return &AuctionComparisonData{score, ret, compare}
 }
 
-func (data *AuctionComparisonData) compareAndAssign(currentScore int) int {
-	if data.compareFunction(currentScore, data.scoreToCompare) {
-		return data.scoreToReturn
-	}
-	return currentScore
-}
-
 // Compose func
 func Compose(currentScore int, data ...*AuctionComparisonData) int {
 	for _, d := range data {
@@ -36,3 +29,10 @@ var LT = func(a, b int) bool { return a <= b }
 
 // GT var
 var GT = func(a, b int) bool { return a >= b }
+
+func (data *AuctionComparisonData) compareAndAssign(currentScore int) int {
+	if data.compareFunction(currentScore, data.scoreToCompare) {
+		return data.scoreToReturn
+	}
+	return currentScore
+}
