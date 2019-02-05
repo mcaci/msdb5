0f8aa7b42f6047fb758e4d72ed5da4cc3da24f9b
Michele Caci <michele.caci@amadeus.com>
2019-02-05 14:07:47 +0100

    Refactor: remove board test for card hand not present in hand after play

--- board/card_play_test.go
+++ board/card_play_test.go
@@ -2,16 +2,6 @@ package board
 
 import "testing"
 
-func TestPlayerPlaysOneCardAndCardIsRemovedFromHand(t *testing.T) {
-	// TODO: fix this test
-	b := New()
-	b.Join("A", "100.0.0.1")
-	b.Play("1", "Coin", "100.0.0.1")
-	if !b.PlayedCards().Has(1) {
-		t.Fatal("Board should have One of Coin as played card")
-	}
-}
-
 func TestPlayerPlaysOneCardAndCardIsOnTheBoard(t *testing.T) {
 	b := New()
 	b.Join("A", "100.0.0.1")
