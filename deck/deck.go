ab944d7276908c183a30e4f36e4dca616c4f9e89
nikiforosFreespirit <michele.caci@gmail.com>
2019-02-09 21:25:46 +0100

    Refactor: use value in deck creation

--- deck/deck.go
+++ deck/deck.go
@@ -15,12 +15,13 @@ func Deck() Cards {
 	var ids Cards
 	rand.Seed(time.Now().UnixNano())
 	ints := rand.Perm(DeckSize)
-	for index := range ints {
-		card, _ := card.ByID(fromZeroBased(ints[index]))
+	for _, cardID := range ints {
+		card, _ := card.ByID(fromZeroBased(cardID))
 		ids.Add(card)
 	}
 	return ids
 }
+
 func fromZeroBased(index int) uint8 {
 	return uint8(index + 1)
 }
