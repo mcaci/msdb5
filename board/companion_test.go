bc2679849799054c16d0e338e972ee3696435bdd
Michele Caci <michele.caci@amadeus.com>
2019-02-11 17:02:54 +0100

    Refactor: nominate player test using card info

--- board/companion_test.go
+++ board/companion_test.go
@@ -1,6 +1,7 @@
 package board
 
 import (
+	"strconv"
 	"testing"
 )
 
@@ -12,11 +13,12 @@ func TestCardNominatedWithOriginInfo(t *testing.T) {
 	}
 }
 
-func TestCardNominatedWithOriginInfo_SetNominatedPlayer(t *testing.T) {
+func TestCardNominatedWithOriginInfo_SetNominatedPlayerA(t *testing.T) {
 	b := New()
 	b.Join("A", "100.1.1.1")
 	b.Join("B", "100.1.1.2")
-	b.Nominate("1", "Coin", "100.1.1.1")
+	card := (*b.Players()[0].Hand())[0]
+	b.Nominate(strconv.Itoa((int)(card.Number())), card.Seed().String(), "100.1.1.2")
 	if b.NominatedPlayer().Name() != "A" {
 		t.Fatal("Nominated player is not A")
 	}
