294276a7bfd74cb65bd809fe8bfefc9493341e3d
Michele Caci <michele.caci@amadeus.com>
2019-02-14 15:37:00 +0100

    Refactor: unexported method in card creation test

--- card/byname_test.go
+++ card/byname_test.go
@@ -6,51 +6,51 @@ import (
 )
 
 func Test1OfCoinIsCreatedCorrectly_NoError(t *testing.T) {
-	NoErrorCheck(t, "1", "Coin")
+	noErrorCheck(t, "1", "Coin")
 }
 
 func Test1OfCoinIsCreatedCorrectly_NumberIs1(t *testing.T) {
-	NumberOfCardCheck(t, "1", "Coin")
+	numberOfCardCheck(t, "1", "Coin")
 }
 
 func Test1OfCoinIsCreatedCorrectly_SeedIsCoin(t *testing.T) {
-	SeedOfCardCheck(t, "1", "Coin")
+	seedOfCardCheck(t, "1", "Coin")
 }
 
 func Test2OfSwordIsCreatedCorrectly_NoError(t *testing.T) {
-	NoErrorCheck(t, "2", "Sword")
+	noErrorCheck(t, "2", "Sword")
 }
 
 func Test2OfSwordIsCreatedCorrectly_NumberIs2(t *testing.T) {
-	NumberOfCardCheck(t, "2", "Sword")
+	numberOfCardCheck(t, "2", "Sword")
 }
 
 func Test2OfSwordIsCreatedCorrectly_SeedIsSword(t *testing.T) {
-	SeedOfCardCheck(t, "2", "Sword")
+	seedOfCardCheck(t, "2", "Sword")
 }
 
 func Test8OfCupIsCreatedCorrectly_NoError(t *testing.T) {
-	NoErrorCheck(t, "8", "Cup")
+	noErrorCheck(t, "8", "Cup")
 }
 
 func Test8OfCupIsCreatedCorrectly_NumberIs8(t *testing.T) {
-	NumberOfCardCheck(t, "8", "Cup")
+	numberOfCardCheck(t, "8", "Cup")
 }
 
 func Test8OfCupIsCreatedCorrectly_SeedIsCup(t *testing.T) {
-	SeedOfCardCheck(t, "8", "Cup")
+	seedOfCardCheck(t, "8", "Cup")
 }
 
 func Test10OfCudgelIsCreatedCorrectly_NoError(t *testing.T) {
-	NoErrorCheck(t, "10", "Cudgel")
+	noErrorCheck(t, "10", "Cudgel")
 }
 
 func Test10OfCudgelIsCreatedCorrectly_NumberIs10(t *testing.T) {
-	NumberOfCardCheck(t, "10", "Cudgel")
+	numberOfCardCheck(t, "10", "Cudgel")
 }
 
 func Test10OfCudgelIsCreatedCorrectly_SeedIsCudgel(t *testing.T) {
-	SeedOfCardCheck(t, "10", "Cudgel")
+	seedOfCardCheck(t, "10", "Cudgel")
 }
 
 func Test15OfCupDoesntExist(t *testing.T) {
@@ -73,21 +73,21 @@ func TestEmptySeedIsIncorrect(t *testing.T) {
 	errorCheck(t, "6", "")
 }
 
-func SeedOfCardCheck(t *testing.T, number, seed string) {
+func seedOfCardCheck(t *testing.T, number, seed string) {
 	check := func(card ID, err error) bool { return card.Seed().String() != seed }
 	if check(ByName(number, seed)) {
 		t.Fatalf("Card's number is not created well from %s and %s", number, seed)
 	}
 }
 
-func NumberOfCardCheck(t *testing.T, number, seed string) {
+func numberOfCardCheck(t *testing.T, number, seed string) {
 	check := func(card ID, err error) bool { return strconv.Itoa(int(card.Number())) != number }
 	if check(ByName(number, seed)) {
 		t.Fatalf("Card's number is not created well from %s and %s", number, seed)
 	}
 }
 
-func NoErrorCheck(t *testing.T, number, seed string) {
+func noErrorCheck(t *testing.T, number, seed string) {
 	check := func(card ID, err error) bool { return err != nil }
 	if check(ByName(number, seed)) {
 		t.Fatalf("An unexpected error was raised")
