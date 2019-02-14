4c6d90281f08b0c5be5d149ddc48243ea61e8cc5
Michele Caci <michele.caci@amadeus.com>
2019-02-14 17:46:09 +0100

    Refactor: remove id_map file

--- card/id.go
+++ card/id.go
@@ -7,12 +7,12 @@ type ID uint8
 
 // Number func
 func (id ID) Number() uint8 {
-	return mapIDToNumber(id)
+	return (uint8(id)-1)%10 + 1
 }
 
 // Seed func
 func (id ID) Seed() Seed {
-	return mapIDToSeed(id)
+	return Seed((uint8(id) - 1) / 10)
 }
 
 func (id ID) String() string {
--- card/id_map.go
+++
@@ -1,9 +0,0 @@
-package card
-
-func mapIDToNumber(index ID) uint8 {
-	return (uint8(index)-1)%10 + 1
-}
-
-func mapIDToSeed(index ID) Seed {
-	return Seed((uint8(index) - 1) / 10)
-}
