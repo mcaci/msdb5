65b83818a8ad590b4521eab378f738d7461a7862
Michele Caci <michele.caci@amadeus.com>
2019-02-14 17:43:41 +0100

    Refactor: remove uncovered code in card creation

--- card/create.go
+++ card/create.go
@@ -6,46 +6,28 @@ import (
 )
 
 // Create func
-func Create(number, seed string) (id ID, err error) {
+func Create(number, seed string) (ID, error) {
 	n, err := nameToNumber(number)
 	if err != nil {
-		return
+		return 0, err
 	}
 	s, err := nameToSeed(seed)
-	if err != nil {
-		return
-	}
-	index, err := mapToID(n, s)
-	if err != nil {
-		return
-	}
-	return ID(index), err
+	return mapToID(n, s), err
 }
 
-func mapToID(number uint8, seed Seed) (index uint8, err error) {
-	index = number + (uint8)(seed)*10
-	if index < 1 {
-		err = errors.New("Index cannot be less than 1")
-	}
-	if index > 40 {
-		err = errors.New("Index cannot be more than 40")
-	}
-	return
+func mapToID(number uint8, seed Seed) ID {
+	return ID(number + (uint8)(seed)*10)
 }
 
 func nameToNumber(number string) (uint8, error) {
 	n, err := strconv.Atoi(number)
-
 	if n > 10 || n < 1 {
 		err = errors.New("Number '" + number + "' is not valid for card")
 	}
 	return uint8(n), err
 }
 
-func nameToSeed(seed string) (Seed, error) {
-	var s Seed
-	var err error
-
+func nameToSeed(seed string) (s Seed, err error) {
 	switch seed {
 	case Coin.String():
 		s = Coin
@@ -58,5 +40,5 @@ func nameToSeed(seed string) (Seed, error) {
 	default:
 		err = errors.New("Seed '" + seed + "' doesn't exist")
 	}
-	return s, err
+	return
 }
