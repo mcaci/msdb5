c76b2e9ce3423d00dabe5eb34bfb8a3587a4583f
Michele Caci <michele.caci@amadeus.com>
2019-02-22 12:02:02 +0100

    Red: test removing moved card from origin is working

--- deck/cards_test.go
+++ deck/cards_test.go
@@ -19,11 +19,20 @@ func TestRemoveCardFromSet(t *testing.T) {
 	}
 }
 
-func TestMoveCards(t *testing.T) {
+func TestMovedCardsAreAddedToDestination(t *testing.T) {
 	playedCards := Cards{2, 3, 4, 5, 6}
 	playerPile := Cards{}
 	playedCards.Move(&playerPile)
 	if len(playerPile) == 0 {
 		t.Fatalf("Cards did not move to player pile")
+	}
+}
+
+func TestMovedCardsAreNotInOriginalSet(t *testing.T) {
+	playedCards := Cards{2, 3, 4, 5, 6}
+	playerPile := Cards{}
+	playedCards.Move(&playerPile)
+	if len(playedCards) != 0 {
+		t.Fatalf("Cards were not moved from played cards")
 	}
 }
