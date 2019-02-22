7a72a1dad264e2cd4e7dec0af8476142e56020d7
Michele Caci <michele.caci@amadeus.com>
2019-02-22 12:14:29 +0100

    Red: test winning player gets his cards

--- board/card_play_test.go
+++ board/card_play_test.go
@@ -29,3 +29,21 @@ func TestWhenPlay5CardsTriggerMove(t *testing.T) {
 		t.Fatal("Board should have triggered the move to winning player")
 	}
 }
+
+func TestWhenPlay5CardsRoundWinnerGetsCards(t *testing.T) {
+	b := New()
+	b.Join("A", "100.0.0.1")
+	b.Join("B", "100.0.0.2")
+	b.Join("C", "100.0.0.3")
+	b.Join("D", "100.0.0.4")
+	b.Join("E", "100.0.0.5")
+	roundWinnerPileSize := len(*b.Players()[2].Pile())
+	b.Play("5", "Coin", "100.0.0.1")
+	b.Play("3", "Cup", "100.0.0.2")
+	b.Play("1", "Coin", "100.0.0.3")
+	b.Play("4", "Sword", "100.0.0.4")
+	b.Play("8", "Cudgel", "100.0.0.5")
+	if len(*b.Players()[2].Pile()) != roundWinnerPileSize+5 {
+		t.Fatal("Winning player didn't get his cards")
+	}
+}
