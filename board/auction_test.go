f0ff171da95fb138ec7ad2afc8e5987e280a0b97
Michele Caci <michele.caci@amadeus.com>
2019-02-22 16:15:52 +0100

    Refactor: extract common init code

--- board/auction_test.go
+++ board/auction_test.go
@@ -4,63 +4,60 @@ import (
 	"testing"
 )
 
+func MockBoard() *Board {
+	testBoard := New()
+	testBoard.Join("A", "100.1.1.1")
+	testBoard.Join("B", "100.1.1.2")
+	return testBoard
+}
+
+func TestRaiseAuctionScoreFirstAssignment(t *testing.T) {
+	testBoard := MockBoard()
+	testBoard.RaiseAuction("61", "100.1.1.1")
+	testPlayerScore(t, testBoard.Players()[0].AuctionScore(), 61)
+}
+
+func TestRaiseAuctionScoreFirstAssignmentShouldBeSuperiorThan61ElseEither61(t *testing.T) {
+	testBoard := MockBoard()
+	testBoard.RaiseAuction("1", "100.1.1.1")
+	testPlayerScore(t, testBoard.Players()[0].AuctionScore(), 61)
+}
+
+func TestInvalidRaiseAuctionScoreFirstAssignmentShouldBeAlways61(t *testing.T) {
+	testBoard := MockBoard()
+	testBoard.RaiseAuction("ciao", "100.1.1.1")
+	testPlayerScore(t, testBoard.Players()[0].AuctionScore(), 61)
+}
+
+func TestRaiseAuctionTo65(t *testing.T) {
+	testBoard := MockBoard()
+	testBoard.RaiseAuction("65", "100.1.1.1")
+	testPlayerScore(t, testBoard.Players()[0].AuctionScore(), 65)
+}
+
+func Test2PlayersRaisingAuction(t *testing.T) {
+	testBoard := MockBoard()
+	testBoard.RaiseAuction("65", "100.1.1.1")
+	testBoard.RaiseAuction("80", "100.1.1.2")
+	testPlayerScore(t, testBoard.Players()[0].AuctionScore(), 65)
+}
+
+func Test2PlayersRaisingAuctionSecondPlayer(t *testing.T) {
+	testBoard := MockBoard()
+	testBoard.RaiseAuction("65", "100.1.1.1")
+	testBoard.RaiseAuction("80", "100.1.1.2")
+	testPlayerScore(t, testBoard.Players()[1].AuctionScore(), 80)
+}
+
+func Test2PlayersRaisingAuctionSecondPlayerDropsWithLowerScore(t *testing.T) {
+	testBoard := MockBoard()
+	testBoard.RaiseAuction("65", "100.1.1.1")
+	testBoard.RaiseAuction("61", "100.1.1.2")
+	testPlayerScore(t, testBoard.Players()[1].AuctionScore(), 0)
+}
+
 func testPlayerScore(t *testing.T, actualScore, expectedScore uint8) {
 	if actualScore != expectedScore {
 		t.Fatalf("Auction score should be set at %d but is %d", expectedScore, actualScore)
 	}
-}
-
-func TestRaiseAuctionScoreWithHostFirstAssignment(t *testing.T) {
-	b := New()
-	b.Join("name", "100.1.1.1")
-	b.RaiseAuction("61", "100.1.1.1")
-	testPlayerScore(t, b.Players()[0].AuctionScore(), 61)
-}
-
-func TestRaiseAuctionScoreWithHostFirstAssignmentShouldBeSuperiorThan61ElseEither61(t *testing.T) {
-	b := New()
-	b.Join("name", "100.1.1.1")
-	b.RaiseAuction("1", "100.1.1.1")
-	testPlayerScore(t, b.Players()[0].AuctionScore(), 61)
-}
-
-func TestInvalidRaiseAuctionScoreWithHostFirstAssignmentShouldBeAlways61(t *testing.T) {
-	b := New()
-	b.Join("name", "100.1.1.1")
-	b.RaiseAuction("ciao", "100.1.1.1")
-	testPlayerScore(t, b.Players()[0].AuctionScore(), 61)
-}
-
-func TestRaiseAuctionTo65WithHost(t *testing.T) {
-	b := New()
-	b.Join("name", "100.1.1.1")
-	b.RaiseAuction("65", "100.1.1.1")
-	testPlayerScore(t, b.Players()[0].AuctionScore(), 65)
-}
-
-func Test2PlayersRaisingAuctionWithHost(t *testing.T) {
-	b := New()
-	b.Join("A", "100.1.1.1")
-	b.Join("B", "100.1.1.2")
-	b.RaiseAuction("65", "100.1.1.1")
-	b.RaiseAuction("80", "100.1.1.2")
-	testPlayerScore(t, b.Players()[0].AuctionScore(), 65)
-}
-
-func Test2PlayersRaisingAuctionWithHostSecondPlayer(t *testing.T) {
-	b := New()
-	b.Join("A", "100.1.1.1")
-	b.Join("B", "100.1.1.2")
-	b.RaiseAuction("65", "100.1.1.1")
-	b.RaiseAuction("80", "100.1.1.2")
-	testPlayerScore(t, b.Players()[1].AuctionScore(), 80)
-}
-
-func Test2PlayersRaisingAuctionWithHostSecondPlayerDropsWithLowerScore(t *testing.T) {
-	b := New()
-	b.Join("A", "100.1.1.1")
-	b.Join("B", "100.1.1.2")
-	b.RaiseAuction("65", "100.1.1.1")
-	b.RaiseAuction("61", "100.1.1.2")
-	testPlayerScore(t, b.Players()[1].AuctionScore(), 0)
 }
