6b45f10785eaa50565f49519b9c2613d7048dd97
Michele Caci <michele.caci@amadeus.com>
2019-02-25 13:33:49 +0100

    Green: add max auction test case

--- board/auction_test.go
+++ board/auction_test.go
@@ -8,6 +8,9 @@ func MockBoard() *Board {
 	testBoard := New()
 	testBoard.Join("A", "100.1.1.1")
 	testBoard.Join("B", "100.1.1.2")
+	testBoard.Join("C", "100.1.1.3")
+	testBoard.Join("D", "100.1.1.4")
+	testBoard.Join("E", "100.1.1.5")
 	return testBoard
 }
 
@@ -34,6 +37,11 @@ func TestRaiseAuctionTo65(t *testing.T) {
 	testBoard.RaiseAuction("65", "100.1.1.1")
 	testPlayerScore(t, testBoard.Players()[0].AuctionScore(), 65)
 }
+func TestRaiseAuctionTo135ShouldStopAt120(t *testing.T) {
+	testBoard := MockBoard()
+	testBoard.RaiseAuction("135", "100.1.1.1")
+	testPlayerScore(t, testBoard.Players()[0].AuctionScore(), 120)
+}
 
 func Test2PlayersRaisingAuction(t *testing.T) {
 	testBoard := MockBoard()
@@ -54,6 +62,16 @@ func Test2PlayersRaisingAuctionSecondPlayerDropsWithLowerScore(t *testing.T) {
 	testBoard.RaiseAuction("65", "100.1.1.1")
 	testBoard.RaiseAuction("61", "100.1.1.2")
 	testPlayerScore(t, testBoard.Players()[1].AuctionScore(), 0)
+}
+
+func Test5PlayersRaisingAuctionAfterTheMaxFifthPlayer(t *testing.T) {
+	testBoard := MockBoard()
+	testBoard.RaiseAuction("65", "100.1.1.1")
+	testBoard.RaiseAuction("80", "100.1.1.2")
+	testBoard.RaiseAuction("81", "100.1.1.3")
+	testBoard.RaiseAuction("100", "100.1.1.4")
+	testBoard.RaiseAuction("121", "100.1.1.5")
+	testPlayerScore(t, testBoard.Players()[4].AuctionScore(), 120)
 }
 
 func testPlayerScore(t *testing.T, actualScore, expectedScore uint8) {
