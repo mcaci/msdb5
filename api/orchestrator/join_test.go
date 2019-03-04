a3b043bc98990d35f0b39502c49d248a176a374b
Michele Caci <michele.caci@amadeus.com>
2019-03-04 12:13:49 +0100

    Red: test switch from joining to auction status

--- api/orchestrator/join_test.go
+++ api/orchestrator/join_test.go
@@ -38,25 +38,36 @@ func TestPlayer2JoinsStatusIsJoining(t *testing.T) {
 	}
 }
 
+func TestPlayer5Joins(t *testing.T) {
+	gameTest := NewGame()
+	gameTest.Join("Michi", "127.0.0.1")
+	gameTest.Join("Mary", "127.0.0.2")
+	gameTest.Join("A", "127.0.0.3")
+	gameTest.Join("gameTester", "127.0.0.4")
+	err := gameTest.Join("C", "127.0.0.5")
+	if err == nil {
+		t.Fatal("All players should have joined correctly")
+	}
+}
+
+func TestPlayer5JoinsAndStatusChangesToAuction(t *testing.T) {
+	gameTest := NewGame()
+	gameTest.Join("Michi", "127.0.0.1")
+	gameTest.Join("Mary", "127.0.0.2")
+	gameTest.Join("A", "127.0.0.3")
+	gameTest.Join("gameTester", "127.0.0.4")
+	gameTest.Join("C", "127.0.0.5")
+	if gameTest.statusInfo != auction {
+		t.Fatal("Status is not correct")
+	}
+}
+
 func TestPlayer6CannotJoin(t *testing.T) {
 	gameTest := NewGame()
 	gameTest.Join("Michi", "127.0.0.1")
 	gameTest.Join("Mary", "127.0.0.2")
 	gameTest.Join("A", "127.0.0.3")
-	gameTest.Join("gameTest", "127.0.0.4")
-	gameTest.Join("C", "127.0.0.5")
-	err := gameTest.Join("Nope", "127.0.0.6")
-	if err == nil {
-		t.Fatal("Player 'Nope' should not be joining as there is no sixth player")
-	}
-}
-
-func TestPlayer6CannotJoinStatusChangesToAuction(t *testing.T) {
-	gameTest := NewGame()
-	gameTest.Join("Michi", "127.0.0.1")
-	gameTest.Join("Mary", "127.0.0.2")
-	gameTest.Join("A", "127.0.0.3")
-	gameTest.Join("gameTest", "127.0.0.4")
+	gameTest.Join("gameTester", "127.0.0.4")
 	gameTest.Join("C", "127.0.0.5")
 	err := gameTest.Join("Nope", "127.0.0.6")
 	if err == nil {
