95dbe7e40f3bd9597a98eba2086c1c6d72bc3a1f
nikiforosFreespirit <michele.caci@gmail.com>
2019-03-05 22:47:01 +0100

    Red: add test for next player to play card after round

--- api/orchestrator/play_card_test.go
+++ api/orchestrator/play_card_test.go
@@ -80,7 +80,7 @@ func TestSecondPlayerCanPlayCardAfterFirstPlayer(t *testing.T) {
 	}
 }
 
-func TestCompleteRound(t *testing.T) {
+func TestCompleteRoundWithNoError(t *testing.T) {
 	gameTest := NewGame()
 	gameTest.Join("A", "100.1.1.1")
 	gameTest.Join("B", "100.1.1.2")
@@ -104,6 +104,33 @@ func TestCompleteRound(t *testing.T) {
 	err := gameTest.Play("3", "Cup", "100.1.1.5")
 	if err != nil {
 		t.Fatal("Expecting full round to happen")
+	}
+}
+
+func TestCompleteRoundWithNextPlayerSelection(t *testing.T) {
+	gameTest := NewGame()
+	gameTest.Join("A", "100.1.1.1")
+	gameTest.Join("B", "100.1.1.2")
+	gameTest.Join("C", "100.1.1.3")
+	gameTest.Join("D", "100.1.1.4")
+	gameTest.Join("E", "100.1.1.5")
+	gameTest.phase = playBriscola
+	gameTest.companion = *companion.New(card.ID(9), gameTest.players[2])
+	pile := deck.Cards{}
+	for i, pl := range gameTest.players {
+		pl.Hand().Move(&pile)
+		pl.Hand().Add(card.ID(2*i + 5))
+		if i > 0 {
+			pl.Fold()
+		}
+	}
+	gameTest.Play("5", "Coin", "100.1.1.1")
+	gameTest.Play("7", "Coin", "100.1.1.2")
+	gameTest.Play("9", "Coin", "100.1.1.3")
+	gameTest.Play("1", "Cup", "100.1.1.4")
+	gameTest.Play("3", "Cup", "100.1.1.5")
+	if gameTest.playerInTurn != 2 {
+		t.Fatal("C should be next player to start")
 	}
 }
 
