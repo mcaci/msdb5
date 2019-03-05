d990c33f2f4ffb22c7305050e2a139b7964db29e
nikiforosFreespirit <michele.caci@gmail.com>
2019-03-05 21:03:08 +0100

    Red: test any other player cannot nominate

--- api/orchestrator/nominate_test.go
+++ api/orchestrator/nominate_test.go
@@ -20,3 +20,15 @@ func TestPlayerInTurnCanNominate(t *testing.T) {
 		t.Fatal("Expecting in turn player to nominate companion card with success")
 	}
 }
+
+func TestAnyOtherPlayerNotInTurnCantNominate(t *testing.T) {
+	gameTest := NewGame()
+	gameTest.Join("A", "100.1.1.1")
+	gameTest.Join("B", "100.1.1.2")
+	gameTest.phase = companionChoice
+	gameTest.playerInTurn = 1
+	err := gameTest.Nominate("2", "Cudgel", "100.1.1.1")
+	if err == nil {
+		t.Fatal("Expecting not in turn player to being able to nominate companion card")
+	}
+}
