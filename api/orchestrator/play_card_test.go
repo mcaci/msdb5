664af3aa67fa6aed7e9bac9cffad615aaf815d65
nikiforosFreespirit <michele.caci@gmail.com>
2019-03-05 22:03:20 +0100

    Red: test second player cannot play if first has not played

--- api/orchestrator/play_card_test.go
+++ api/orchestrator/play_card_test.go
@@ -12,7 +12,7 @@ func TestPlayerCannotPlayCardsIfPhaseIsNotPlay(t *testing.T) {
 	}
 }
 
-func TestPlaysOwnedCard(t *testing.T) {
+func TestPlayerPlaysOwnedCard(t *testing.T) {
 	gameTest := NewGame()
 	gameTest.Join("A", "100.1.1.1")
 	gameTest.players[0].Hand().Add(23)
@@ -20,5 +20,18 @@ func TestPlaysOwnedCard(t *testing.T) {
 	err := gameTest.Play("3", "Sword", "100.1.1.1")
 	if err != nil {
 		t.Fatal("Play card action not expected at beginning of game")
+	}
+}
+
+func TestSecondPlayerCannotPlayCardBeforeFirstPlayer(t *testing.T) {
+	gameTest := NewGame()
+	gameTest.Join("A", "100.1.1.1")
+	gameTest.Join("B", "100.1.1.2")
+	gameTest.phase = playBriscola
+	gameTest.players[0].Hand().Add(34)
+	gameTest.players[1].Hand().Add(15)
+	err := gameTest.Play("5", "Cup", "100.1.1.2")
+	if err == nil {
+		t.Fatal("Expecting error for second player not being able to play cards before first player has")
 	}
 }
