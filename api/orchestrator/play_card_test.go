f6ff4a7836cfb46b947113b573a76c2b8e87daeb
nikiforosFreespirit <michele.caci@gmail.com>
2019-03-05 22:07:25 +0100

    Red: second player can play card after first one

--- api/orchestrator/play_card_test.go
+++ api/orchestrator/play_card_test.go
@@ -35,3 +35,17 @@ func TestSecondPlayerCannotPlayCardBeforeFirstPlayer(t *testing.T) {
 		t.Fatal("Expecting error for second player not being able to play cards before first player has")
 	}
 }
+
+func TestSecondPlayerCanPlayCardAfterFirstPlayer(t *testing.T) {
+	gameTest := NewGame()
+	gameTest.Join("A", "100.1.1.1")
+	gameTest.Join("B", "100.1.1.2")
+	gameTest.phase = playBriscola
+	gameTest.players[0].Hand().Add(34)
+	gameTest.players[1].Hand().Add(15)
+	gameTest.Play("4", "Cudgel", "100.1.1.1")
+	err := gameTest.Play("5", "Cup", "100.1.1.2")
+	if err != nil {
+		t.Fatal("Expecting second player being able to play cards after first player has")
+	}
+}
