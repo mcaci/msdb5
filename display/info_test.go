90d373004e731c7f25d7588577a7fd2529f9b4a6
nikiforosFreespirit <michele.caci@gmail.com>
2019-03-08 22:53:04 +0100

    Refactor: remove useless commented code

--- display/info_test.go
+++ display/info_test.go
@@ -25,28 +25,3 @@ func TestPrintAllOfEmptyDataIsEmpty(t *testing.T) {
 		t.Fatal("unexpected output")
 	}
 }
-
-// type mockStringer uint8
-
-// func (m mockStringer) String() string {
-// 	return "1"
-// }
-
-// func TestToStringOfMockData(t *testing.T) {
-// 	var info1 mockStringer
-// 	var info2 mockStringer
-// 	if ToString(info1, info2) != "1 1 " {
-// 		t.Fatal("unexpected output")
-// 	}
-// }
-
-// type mockStringers []*mockStringer
-
-// func TestToStringOfTypedMockData(t *testing.T) {
-// 	var info1 mockStringer
-// 	var info2 mockStringer
-// 	infos := mockStringers{&info1, &info2}
-// 	if ToString((*infos)...) != "1 1 " {
-// 		t.Fatal("unexpected output")
-// 	}
-// }
