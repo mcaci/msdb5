d4e754e6767e25d0b892c90c64b5109c937f974d
nikiforosFreespirit <michele.caci@gmail.com>
2019-03-12 23:25:38 +0100

    Refactor: remove duplications in join test

--- player/join_test.go
+++ player/join_test.go
@@ -2,33 +2,32 @@ package player
 
 import "testing"
 
-func TestJoinPlayerSameToItself(t *testing.T) {
+var p *Player
+
+func init() {
 	p := New()
 	p.Join("Michi", "127.0.0.1")
+}
+
+func TestJoinPlayerSameToItself(t *testing.T) {
 	if !p.IsSame(p) {
 		t.Fatal("P should be equal to itself")
 	}
 }
 
 func TestJoinPlayerName(t *testing.T) {
-	p := New()
-	p.Join("Michi", "127.0.0.1")
 	if !p.IsName("Michi") {
 		t.Fatal("Unexpected name")
 	}
 }
 
 func TestJoinPlayerNameNotEmpty(t *testing.T) {
-	p := New()
-	p.Join("Michi", "127.0.0.1")
 	if p.IsNameEmpty() {
-		t.Fatal("Unexpected name")
+		t.Fatal("Unexpected name being empty")
 	}
 }
 
 func TestJoinPlayerHost(t *testing.T) {
-	p := New()
-	p.Join("Michi", "127.0.0.1")
 	if !p.IsSameHost("127.0.0.1") {
 		t.Fatal("Unexpected host")
 	}
