eecc0134ee7245933c597ad35e93e4152589bd55
nikiforosFreespirit <michele.caci@gmail.com>
2019-03-13 22:13:50 +0100

    Legacy: implement stringer for card

--- card/id.go
+++ card/id.go
@@ -1,5 +1,7 @@
 package card
 
+import "fmt"
+
 // ID is the id of a card from 1 to 40
 type ID uint8
 
@@ -11,4 +13,8 @@ func (id ID) Number() uint8 {
 // Seed func
 func (id ID) Seed() Seed {
 	return Seed((uint8(id) - 1) / 10)
+}
+
+func (id ID) String() string {
+	return fmt.Sprintf("%d of %s", id.Number(), id.Seed())
 }
