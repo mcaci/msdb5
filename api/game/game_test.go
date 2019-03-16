b52ba976cd08bef367db377669424098b024ea39
nikiforosFreespirit <michele.caci@gmail.com>
2019-03-16 13:24:03 +0100

    Green: fix unit tests post merge

--- api/game/game_test.go
+++ api/game/game_test.go
@@ -8,42 +8,42 @@ import (
 )
 
 func TestBoardHas5Player(t *testing.T) {
-	if gameTest := NewGame(); gameTest.Players() == nil {
+	if gameTest := NewGame(false); gameTest.Players() == nil {
 		t.Fatal("The board has no Player")
 	}
 }
 func TestGameHasNoPlayerInTurnAtStart(t *testing.T) {
-	if gameTest := NewGame(); gameTest.PlayerInTurn() == nil {
+	if gameTest := NewGame(false); gameTest.PlayerInTurn() == nil {
 		t.Fatal("The board has no Player in turn")
 	}
 }
 
 func TestPlayer1Has8Cards(t *testing.T) {
-	if gameTest := NewGame(); len(*gameTest.players[0].Hand()) != 8 {
+	if gameTest := NewGame(false); len(*gameTest.players[0].Hand()) != 8 {
 		t.Fatalf("Player has %d cards", len(*gameTest.players[0].Hand()))
 	}
 }
 
 func TestBoardHasASetOfPlayedCards(t *testing.T) {
-	if gameTest := NewGame(); gameTest.board.PlayedCards() == nil {
+	if gameTest := NewGame(false); gameTest.board.PlayedCards() == nil {
 		t.Fatal("The board has no set of played cards")
 	}
 }
 
 func TestBoardsEmptySetOfPlayedCardsContainsNoCards(t *testing.T) {
-	if gameTest := NewGame(); len(*gameTest.board.PlayedCards()) > 0 {
+	if gameTest := NewGame(false); len(*gameTest.board.PlayedCards()) > 0 {
 		t.Fatal("The deck should be empty at this point")
 	}
 }
 
 func TestBoardAuctionScoreAtCreationIs0(t *testing.T) {
-	if gameTest := NewGame(); gameTest.Board().AuctionScore() != 0 {
+	if gameTest := NewGame(false); gameTest.Board().AuctionScore() != 0 {
 		t.Fatalf("Auction score for a new board should be 0 but is %d", gameTest.board.AuctionScore())
 	}
 }
 
 func TestNextPhase(t *testing.T) {
-	gameTest := NewGame()
+	gameTest := NewGame(false)
 	gameTest.NextPhase()
 	if gameTest.CurrentPhase() != 1 {
 		t.Fatal("Current phase should be 1")
@@ -51,7 +51,7 @@ func TestNextPhase(t *testing.T) {
 }
 
 func TestSetCompanionAndBriscolaSeed(t *testing.T) {
-	testGame := NewGame()
+	testGame := NewGame(false)
 	testGame.SetCompanion(1, player.New())
 	if testGame.BriscolaSeed() != card.Coin {
 		t.Fatal("Expecting coin as briscola")
@@ -59,7 +59,7 @@ func TestSetCompanionAndBriscolaSeed(t *testing.T) {
 }
 
 func TestSetCompanionAndPlayerReference(t *testing.T) {
-	testGame := NewGame()
+	testGame := NewGame(false)
 	testGame.SetCompanion(1, player.New())
 	if testGame.Companion() == nil {
 		t.Fatal("Companion to be set")
@@ -67,7 +67,7 @@ func TestSetCompanionAndPlayerReference(t *testing.T) {
 }
 
 func TestNextPlayer(t *testing.T) {
-	testGame := NewGame()
+	testGame := NewGame(false)
 	testGame.NextPlayer(func (uint8) uint8 {return 3})
 	if testGame.playerInTurn != 3 {
 		t.Fatal("current player index should be 3")
