9c1a019e4651edb50df569dcc28c04d392a9640e
nikiforosFreespirit <michele.caci@gmail.com>
2019-03-16 20:00:03 +0100

    Red: test side deck size when used

--- api/game/game_with_side_test.go
+++ api/game/game_with_side_test.go
@@ -21,3 +21,15 @@ func TestPlayer1Has7Cards(t *testing.T) {
 		t.Fatalf("Player has %d cards", len(*gameTest.players[0].Hand()))
 	}
 }
+
+func TestPlayer4Has7Cards(t *testing.T) {
+	if gameTest := NewGame(true); len(*gameTest.players[4].Hand()) != 7 {
+		t.Fatalf("Player has %d cards", len(*gameTest.players[4].Hand()))
+	}
+}
+
+func TestSideDeckHas5CardsWhenPresent(t *testing.T) {
+	if gameTest := NewGame(true); len(gameTest.side) != 5 {
+		t.Fatalf("Side deck has %d cards", len(gameTest.side))
+	}
+}
