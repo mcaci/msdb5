3ba5fc46a77c1cdb0b815b7017984217cda5d9f0
Michele Caci <michele.caci@amadeus.com>
2019-03-19 18:53:59 +0100

    Gree: add implementation for exchange cards phase

--- api/action/exchange_cards.go
+++ api/action/exchange_cards.go
@@ -1,6 +1,9 @@
 package action
 
 import (
+	"strconv"
+	"strings"
+
 	"github.com/nikiforosFreespirit/msdb5/api/game"
 	"github.com/nikiforosFreespirit/msdb5/player"
 	"github.com/nikiforosFreespirit/msdb5/playerset"
@@ -8,21 +11,24 @@ import (
 
 type ExchangeCardsStruct struct {
 	request, origin string
+	playerInTurn    *player.Player
 }
 
-func NewExchangeCards(request, origin string) Action {
-	return &ExchangeCardsStruct{request, origin}
+func NewExchangeCards(request, origin string, playerInTurn *player.Player) Action {
+	return &ExchangeCardsStruct{request, origin, playerInTurn}
 }
-
-func (ecs ExchangeCardsStruct) Phase() game.Phase                   { return game.ExchangingCards }
-func (ecs ExchangeCardsStruct) Find(p *player.Player) bool          { return true }
+func (ecs ExchangeCardsStruct) Phase() game.Phase { return game.ExchangingCards }
+func (ecs ExchangeCardsStruct) Find(p *player.Player) bool {
+	return p.IsExpectedPlayer(ecs.playerInTurn, ecs.origin)
+}
 func (ecs ExchangeCardsStruct) Do(p *player.Player) error           { return nil }
 func (ecs ExchangeCardsStruct) NextPlayer(playerInTurn uint8) uint8 { return playerInTurn }
 func (ecs ExchangeCardsStruct) NextPhase(players playerset.Players, predicate PlayerPredicate) game.Phase {
-	condition := true
-	if condition {
-		return game.ExchangingCards
+	data := strings.Split(ecs.request, "#")
+	number, err := strconv.Atoi(data[1])
+	if number == 0 || err != nil {
+		return game.ChosingCompanion
 	}
-	return game.ChosingCompanion
+	return game.ExchangingCards
 }
 func (ecs ExchangeCardsStruct) NextPhasePlayerInfo(p *player.Player) bool { return true }
--- api/action/exchange_cards_test.go
+++ api/action/exchange_cards_test.go
@@ -1,12 +1,55 @@
 package action
 
 import (
-	"github.com/nikiforosFreespirit/msdb5/api/game"
 	"testing"
+
+	"github.com/nikiforosFreespirit/msdb5/api/game"
+	"github.com/nikiforosFreespirit/msdb5/player"
+	"github.com/nikiforosFreespirit/msdb5/playerset"
 )
 
 func TestExchangeCardsPhase(t *testing.T) {
-	if testObject := NewExchangeCards("", ""); testObject.Phase() != game.ExchangingCards {
+	if testObject := NewExchangeCards("", "", nil); testObject.Phase() != game.ExchangingCards {
 		t.Fatalf("Unexpected phase")
+	}
+}
+
+func TestExchangeCardsFindsPlayerInTurn(t *testing.T) {
+	testPlayer := player.New()
+	testPlayer.Join("A", "127.0.0.3")
+	if testObject := NewExchangeCards("", "127.0.0.3", testPlayer); !testObject.Find(testPlayer) {
+		t.Fatalf("Unexpected player")
+	}
+}
+
+func TestExchangeCardsNextPlayerOf2is2(t *testing.T) {
+	if testObject := NewExchangeCards("", "", nil); testObject.NextPlayer(2) != 2 {
+		t.Fatalf("Next player should be 2")
+	}
+}
+
+func TestExchangeCardsNextPlayerOf4is4(t *testing.T) {
+	if testObject := NewExchangeCards("", "", nil); testObject.NextPlayer(4) != 4 {
+		t.Fatalf("Next player should be 1")
+	}
+}
+
+func TestExchangeCardsNextPhaseWhenInputIs0(t *testing.T) {
+	testPlayers := playerset.Players{player.New()}
+	if testObject := NewExchangeCards("#0", "", nil); game.ChosingCompanion != testObject.NextPhase(testPlayers, testObject) {
+		t.Fatalf("Should change phase when 0 is in the request")
+	}
+}
+
+func TestExchangeCardsNextPhaseWhenInputIsNot0(t *testing.T) {
+	testPlayers := playerset.Players{player.New()}
+	if testObject := NewExchangeCards("#1", "", nil); game.ExchangingCards != testObject.NextPhase(testPlayers, testObject) {
+		t.Fatalf("Should not change phase when 1 is in the request")
+	}
+}
+
+func TestExchangeCardsNextPhaseWithPlayersWithNonEmptyNameIsFalse(t *testing.T) {
+	if testObject := NewExchangeCards("", "", nil); !testObject.NextPhasePlayerInfo(player.New()) {
+		t.Fatalf("Should always be true")
 	}
 }
