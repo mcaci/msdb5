8da6006aa391175ed05f9258841cabfc2779c7ae
nikiforosFreespirit <michele.caci@gmail.com>
2019-03-19 23:40:26 +0100

    Red: test exchange of cards

--- api/action/exchange_cards_do_test.go
+++ api/action/exchange_cards_do_test.go
@@ -3,6 +3,8 @@ package action
 import (
 	"testing"
 
+	"github.com/nikiforosFreespirit/msdb5/board"
+	"github.com/nikiforosFreespirit/msdb5/deck"
 	"github.com/nikiforosFreespirit/msdb5/player"
 )
 
@@ -10,7 +12,20 @@ func TestExchangeDoNoErr(t *testing.T) {
 	testObject := NewExchangeCards("Exchange#1#Coin", "127.0.0.2", nil)
 	testPlayer := player.New()
 	err := testObject.Do(testPlayer)
-	if err == nil {
-		t.Fatalf("Unexpected error when exchanging cards phase")
+	if err != nil {
+		t.Fatal("Unexpected error when exchanging cards phase")
+	}
+}
+
+func TestExchangeOneCardPicksFirst(t *testing.T) {
+	testPlayer := player.New()
+	testPlayer.Hand().Add(1)
+	testPlayedCards := deck.Cards{2, 3, 4, 6, 7}
+	testBoard := board.New()
+	testBoard.SideDeck().Add(testPlayedCards...)
+	testObject := NewExchangeCards("Exchange#1#Coin", "127.0.0.2", testPlayer)
+	testObject.Do(testPlayer)
+	if !testPlayer.Hand().Has(2) {
+		t.Fatalf("Cards were not exchanged properly. Current hand: %v", testPlayer.Hand())
 	}
 }
