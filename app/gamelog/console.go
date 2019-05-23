ac3c7f73ed0d89035e068cdbfcdf496d690e6fd7
nikiforosFreespirit <michele.caci@gmail.com>
2019-05-23 22:55:59 +0200

    Log: improve console log with player info

--- app/gamelog/console.go
+++ app/gamelog/console.go
@@ -7,5 +7,6 @@ func ToConsole(gameInfo informer, request string, err error) {
 	log.Printf("New Action by %s\n", gameInfo.CurrentPlayer().Name())
 	log.Printf("Action is %s\n", request)
 	log.Printf("Any error raised: %+v\n", err)
+	log.Printf("Player info after action: %+v\n", gameInfo.CurrentPlayer())
 	log.Printf("Game info after action: %+v\n", gameInfo)
 }
