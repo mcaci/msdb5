6aec869358456cffcac5bb06ba7b8a804f8cd11b
nikiforosFreespirit <michele.caci@gmail.com>
2019-07-14 11:08:55 +0200

    Green: test and refactor auction side cards

--- app/play/auction.go
+++ app/play/auction.go
@@ -6,18 +6,5 @@ import (
 
 // SideCardsToDisplay func
 func SideCardsToDisplay(score auction.Score) uint8 {
-	var cardsToShow uint8
-	switch {
-	case score < 90:
-		cardsToShow = 0
-	case score < 100:
-		cardsToShow = 1
-	case score < 110:
-		cardsToShow = 2
-	case score < 120:
-		cardsToShow = 3
-	default:
-		cardsToShow = 5
-	}
-	return cardsToShow
+	return uint8(score)/90 + uint8(score)/100 + uint8(score)/110 + uint8(score)/120 + uint8(score)/120
 }
