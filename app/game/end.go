b6da41e32a5732c41adf23e7800bce52afa1968a
nikiforosFreespirit <michele.caci@gmail.com>
2019-07-14 16:43:00 +0200

    Refactor: move collect cards operation close

--- app/game/end.go
+++ app/game/end.go
@@ -4,9 +4,7 @@ import (
 	"container/list"
 
 	"golang.org/x/text/language"
-	"golang.org/x/text/message"
 
-	"github.com/mcaci/msdb5/app/track"
 	"github.com/mcaci/msdb5/dom/card"
 	"github.com/mcaci/msdb5/dom/deck"
 	"github.com/mcaci/msdb5/dom/player"
@@ -43,32 +41,4 @@ func predict(g allInformer, roundsBefore, limit uint8) bool {
 	}
 	collect(g, others)
 	return true
-}
-
-type collector interface {
-	Caller() *player.Player
-	Companion() *player.Player
-	Players() team.Players
-	LastPlaying() *list.List
-	Lang() language.Tag
-	IsSideUsed() bool
-	SideDeck() *deck.Cards
-}
-
-func collect(g collector, others bool) {
-	p := g.Caller()
-	printer := message.NewPrinter(g.Lang())
-	team := printer.Sprintf("Callers")
-	if others {
-		_, p = g.Players().Find(func(p *player.Player) bool { return p == g.Caller() || p == g.Companion() })
-		team = printer.Sprintf("Others")
-	}
-	for _, pl := range g.Players() {
-		p.Collect(pl.Hand())
-		printer.Fprintf(pl, "The end - %s team has all briscola cards", team)
-	}
-	if g.IsSideUsed() {
-		p.Collect(g.SideDeck())
-	}
-	track.Player(g.LastPlaying(), p)
 }
--- app/game/post_request.go
+++ app/game/post_request.go
@@ -1,10 +1,16 @@
 package game
 
 import (
+	"container/list"
+
 	"github.com/mcaci/msdb5/app/phase"
+	"github.com/mcaci/msdb5/app/track"
 	"github.com/mcaci/msdb5/dom/briscola"
+	"github.com/mcaci/msdb5/dom/deck"
 	"github.com/mcaci/msdb5/dom/player"
 	"github.com/mcaci/msdb5/dom/team"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
 )
 
 func postRequest(g roundInformer, rq requestInformer) {
@@ -15,15 +21,51 @@ func postRequest(g roundInformer, rq requestInformer) {
 		if !roundHasEnded {
 			break
 		}
-		playerIndex, _ := g.Players().Find(func(pl *player.Player) bool { return pl == g.CurrentPlayer() })
-		winningCardIndex := briscola.IndexOfWinningCard(*g.PlayedCards(), g.Briscola())
-		winnerIndex := (playerIndex + int(winningCardIndex) + 1) % 5
-		g.Players()[winnerIndex].Collect(g.PlayedCards())
+		winnerIndex := roundWinnerIndex(g)
+		collectingPlayer := g.Players()[winnerIndex]
+		collectingPlayer.Collect(g.PlayedCards())
 		if !(team.Count(g.Players(), player.IsHandEmpty) == 5 && g.IsSideUsed()) {
 			break
 		}
 		side := g.SideDeck()
-		g.Players()[winnerIndex].Collect(side)
+		collectingPlayer.Collect(side)
 		side.Clear()
 	}
+}
+
+type collector interface {
+	Caller() *player.Player
+	Companion() *player.Player
+	Players() team.Players
+	LastPlaying() *list.List
+	Lang() language.Tag
+	IsSideUsed() bool
+	SideDeck() *deck.Cards
+}
+
+func collect(g collector, others bool) {
+	collectingPlayer := g.Caller()
+	printer := message.NewPrinter(g.Lang())
+	team := printer.Sprintf("Callers")
+	if others {
+		_, collectingPlayer = g.Players().Find(func(p *player.Player) bool { return p == g.Caller() || p == g.Companion() })
+		team = printer.Sprintf("Others")
+	}
+	for _, pl := range g.Players() {
+		collectingPlayer.Collect(pl.Hand())
+		printer.Fprintf(pl, "The end - %s team has all briscola cards", team)
+	}
+	track.Player(g.LastPlaying(), collectingPlayer)
+	if !g.IsSideUsed() {
+		return
+	}
+	collectingPlayer.Collect(g.SideDeck())
+	g.SideDeck().Clear()
+}
+
+func roundWinnerIndex(g roundInformer) uint8 {
+	playerIndex, _ := g.Players().Find(func(pl *player.Player) bool { return pl == g.CurrentPlayer() })
+	winningCardIndex := briscola.IndexOfWinningCard(*g.PlayedCards(), g.Briscola())
+	winnerIndex := (playerIndex + int(winningCardIndex) + 1) % 5
+	return uint8(winnerIndex)
 }
