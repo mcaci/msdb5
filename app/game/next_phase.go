4a14a6e7046deccc998a8dd9d37dff6f6b5cd25f
mcaci <michele.caci@gmail.com>
2019-08-18 08:58:33 +0200

    Refactor: move end into next phase

--- app/game/next_phase.go
+++ app/game/next_phase.go
@@ -1,7 +1,9 @@
 package game
 
 import (
+	"github.com/mcaci/ita-cards/card"
 	"github.com/mcaci/msdb5/app/phase"
+	"github.com/mcaci/msdb5/dom/briscola"
 	"github.com/mcaci/msdb5/dom/player"
 	"github.com/mcaci/msdb5/dom/team"
 )
@@ -31,4 +33,35 @@ func nextPhase(g roundInformer, rq interface{ Value() string }) phase.ID {
 
 func checkAllWithEmptyHands(g interface{ Players() team.Players }) bool {
 	return team.Count(g.Players(), player.IsHandEmpty) == 5
+}
+
+type predictor interface {
+	Briscola() card.Item
+	Caller() *player.Player
+	Companion() *player.Player
+	IsRoundOngoing() bool
+	Players() team.Players
+}
+
+func predict(g predictor, roundsBefore, limit uint8) bool {
+	highbriscolaCard := briscola.Serie(g.Briscola())
+	var callersHave, othersHave bool
+	var roundsChecked uint8
+	for _, card := range highbriscolaCard {
+		if roundsChecked == limit {
+			break
+		}
+		_, p := g.Players().Find(player.IsCardInHand(card))
+		if p == nil { // no one has card
+			continue
+		}
+		isPlayerInCallers := p == g.Caller() || p == g.Companion()
+		callersHave = callersHave || isPlayerInCallers
+		othersHave = othersHave || !isPlayerInCallers
+		if callersHave == othersHave {
+			break
+		}
+		roundsChecked++
+	}
+	return !g.IsRoundOngoing() && roundsBefore <= limit && callersHave != othersHave
 }
