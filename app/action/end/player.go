16a3fbe9e3bcc14c0dc9140b00e67b6ee323c089
mcaci <michele.caci@gmail.com>
2019-08-31 10:20:34 +0200

    Refactor: remove all output notification

---
+++ app/action/end/player.go
@@ -0,0 +1,27 @@
+package end
+
+import (
+	"github.com/mcaci/ita-cards/card"
+	"github.com/mcaci/msdb5/dom/briscola"
+	"github.com/mcaci/msdb5/dom/player"
+	"github.com/mcaci/msdb5/dom/team"
+)
+
+type currentPlayerProvider interface {
+	CurrentPlayer() *player.Player
+	Players() team.Players
+	Briscola() card.Item
+}
+
+func Player(g currentPlayerProvider) *player.Player {
+	lastPl := g.CurrentPlayer()
+	for _, card := range briscola.Serie(g.Briscola()) {
+		_, p := g.Players().Find(player.IsCardInHand(card))
+		if p == nil { // no one has card
+			continue
+		}
+		lastPl = p
+		break
+	}
+	return lastPl
+}
--- app/game/process.go
+++ app/game/process.go
@@ -1,135 +1,68 @@
 package game
 
 import (
-	"fmt"
-	"os"
-
-	"github.com/mcaci/ita-cards/card"
-
 	"github.com/mcaci/msdb5/app/action"
 	"github.com/mcaci/msdb5/app/action/collect"
 	"github.com/mcaci/msdb5/app/input"
-	"github.com/mcaci/msdb5/app/msg"
 	"github.com/mcaci/msdb5/app/next"
 	"github.com/mcaci/msdb5/app/phase"
-	"github.com/mcaci/msdb5/app/score"
 	"github.com/mcaci/msdb5/app/track"
-	"github.com/mcaci/msdb5/dom/briscola"
-	"github.com/mcaci/msdb5/dom/player"
 	"github.com/mcaci/msdb5/dom/team"
-	"golang.org/x/text/message"
 )
 
-func (r *report) noErrExec(s senderInfo, inputRequest string, err error) {
-	if r.err == nil {
-		r.error(s, inputRequest, err)
-	}
-}
-
 // Process func
-func (g *Game) Process(inputRequest, origin string) []PlMsg {
-	printer := message.NewPrinter(g.Lang())
-	r := report{}
-
+func (g *Game) Process(inputRequest, origin string) (err error) {
 	// verify phase step
-	if r.err == nil {
-		s := senderInfo{g.Players(), origin}
-		phInfo := phaseInfo{g.Phase(), input.Command(inputRequest)}
-		r.error(s, inputRequest, phase.Check(phInfo))
+	s := senderInfo{g.Players(), origin}
+	phInfo := phaseInfo{g.Phase(), input.Command(inputRequest)}
+	err = phase.Check(phInfo)
+	if err != nil {
+		return err
 	}
 
 	// verify player step
-	if r.err == nil {
-		s := senderInfo{g.Players(), origin}
-		es := expectedSenderInfo{s, g.CurrentPlayer()}
-		r.error(es, inputRequest, team.CheckOrigin(es))
+	es := expectedSenderInfo{s, g.CurrentPlayer()}
+	err = team.CheckOrigin(es)
+	if err != nil {
+		return err
 	}
 
 	// play step
-	if r.err == nil {
-		c, cerr := input.Card(inputRequest)
-		gInfo := gameRound{g, c, cerr, input.Value(inputRequest)}
-		s := senderInfo{g.Players(), origin}
-		r.error(s, inputRequest, action.Play(gInfo))
-	}
-
-	if r.err != nil {
-		return r.reports
-	}
-
-	if g.isToShow {
-		for _, pl := range g.Players() {
-			r.msg(pl, printer.Sprintf("Side deck section: (%s)\n", msg.TranslateCards(g.sideSubset, printer)))
-		}
+	c, cerr := input.Card(inputRequest)
+	gInfo := gameRound{g, c, cerr, input.Value(inputRequest)}
+	err = action.Play(gInfo)
+	if err != nil {
+		return err
 	}
 
 	// end round: next player
 	plInfo := next.NewPlInfo(g.Phase(), g.Players(), g.PlayedCards(), g.Briscola(),
 		len(*g.SideDeck()) > 0, len(*g.PlayedCards()) < 5, origin)
-	track.Player(g.LastPlaying(), next.Player(plInfo))
+	nextPl := next.Player(plInfo)
+	track.Player(g.LastPlaying(), nextPl)
 	if g.Phase() == phase.PlayingCards {
 		collect.Played(collect.NewInfo(g.CurrentPlayer(), g.PlayedCards()))
 	}
 
 	// end round: next phase
-	phInfo := next.NewPhInfo(g.Phase(), g.Players(), g.Caller(), g.Companion(), g.Briscola(),
+	nextPhInfo := next.NewPhInfo(g.Phase(), g.Players(), g.Caller(), g.Companion(), g.Briscola(),
 		len(*g.SideDeck()) > 0, len(*g.PlayedCards()) == 0, input.Value(inputRequest))
-	g.setPhase(next.Phase(phInfo))
-
-	// send logs
-	senderPlayer := team.Sender(senderInfo{g.Players(), origin})
-	r.msg(os.Stdout, fmt.Sprintf("New Action by %s: %s\nSender info: %+v\nGame info: %+v\n", senderPlayer.Name(), inputRequest, senderPlayer, g))
-	for _, pl := range g.Players() {
-		r.msg(pl, "-----")
-	}
-	r.msg(g.LastPlayer(), msg.CreateInGameMsg(g, g.LastPlayer()))
-	gameStatusMsg := msg.TranslateGameStatus(g, printer)
-	for _, pl := range g.Players() {
-		r.msg(pl, gameStatusMsg)
-	}
-	r.msg(g.CurrentPlayer(), msg.CreateInGameMsg(g, g.CurrentPlayer()))
+	g.setPhase(next.Phase(nextPhInfo))
 
 	if g.phase != phase.End {
-		return r.reports
+		return err
 	}
 
-	// process end gamell
-	endMsg := msg.TranslateTeam(lastPlayer(g), g, printer)
-	for _, pl := range g.Players() {
-		r.msg(pl, endMsg)
-	}
+	// process end game
 	// last round winner collects all cards
 	collect.All(collect.NewAllInfo(g.CurrentPlayer(), g.SideDeck(), g.Players()))
 
-	// compute score
-	pilers := make([]score.Piler, len(g.Players()))
-	for i, p := range g.Players() {
-		pilers[i] = p
-	}
-	scoreTeam1, scoreTeam2 := score.Calc(g.Caller(), g.Companion(), pilers, briscola.Points)
-	scoreMsg := printer.Sprintf("The end - Callers: %d; Others: %d", scoreTeam1, scoreTeam2)
-	for _, pl := range g.Players() {
-		r.msg(pl, scoreMsg)
-	}
-	r.msg(g.handleMLData()) // placeholder for ml data
-	return r.reports
-}
+	// compute score (output data)
+	// pilers := make([]score.Piler, len(g.Players()))
+	// for i, p := range g.Players() {
+	// 	pilers[i] = p
+	// }
+	// scoreTeam1, scoreTeam2 := score.Calc(g.Caller(), g.Companion(), pilers, briscola.Points)
 
-type currentPlayerProvider interface {
-	CurrentPlayer() *player.Player
-	Players() team.Players
-	Briscola() card.Item
-}
-
-func lastPlayer(g currentPlayerProvider) *player.Player {
-	lastPl := g.CurrentPlayer()
-	for _, card := range briscola.Serie(g.Briscola()) {
-		_, p := g.Players().Find(player.IsCardInHand(card))
-		if p == nil { // no one has card
-			continue
-		}
-		lastPl = p
-		break
-	}
-	return lastPl
+	return err
 }
