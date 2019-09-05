f3ad565bcffe7d093510daba07b0913586e1a91c
mcaci <michele.caci@gmail.com>
2019-09-05 13:44:15 +0200

    Refactor: reintegrate io.writestring in ml code

--- app/msg/informer_ml.go
+++ app/msg/informer_ml.go
@@ -9,20 +9,17 @@ import (
 )
 
 func toML(g roundInformer) {
-	var dest io.Writer
-	var text string
 	f, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
-		dest, text = os.Stdout, err.Error()
+		io.WriteString(os.Stdout, err.Error())
 	}
 	defer f.Close()
 	switch g.Phase() {
 	case phase.ChoosingCompanion:
-		dest, text = f, fmt.Sprintf("%s, %s, %d\n", g.CurrentPlayer().Name(), g.Companion().Name(), *(g.AuctionScore()))
+		io.WriteString(f, fmt.Sprintf("%s, %s, %d\n", g.CurrentPlayer().Name(), g.Companion().Name(), *(g.AuctionScore())))
 	case phase.PlayingCards:
-		dest, text = f, fmt.Sprintf("%s, %d\n", g.CurrentPlayer().Name(), g.PlayedCard())
+		io.WriteString(f, fmt.Sprintf("%s, %d\n", g.CurrentPlayer().Name(), g.PlayedCard()))
 	case phase.End:
-		dest, text = f, fmt.Sprintf("%s\n", g.CurrentPlayer().Name())
+		io.WriteString(f, fmt.Sprintf("%s\n", g.CurrentPlayer().Name()))
 	}
-	io.WriteString(dest, text)
 }
