6c5accf00fe4d9e60b10c1c426df407f38917718
mcaci <michele.caci@gmail.com>
2019-10-02 11:28:10 +0200

    Refactor: remove dependency in briscola pkg

--- dom/briscola/points.go
+++ dom/briscola/points.go
@@ -1,9 +1,7 @@
 package briscola
 
-import "github.com/mcaci/ita-cards/card"
-
 // Points func
-func Points(id card.Item) uint8 {
+func Points(valuer interface{ Number() uint8 }) uint8 {
 	var points = map[uint8]uint8{1: 11, 3: 10, 8: 2, 9: 3, 10: 4}
-	return points[id.Number()]
+	return points[valuer.Number()]
 }
