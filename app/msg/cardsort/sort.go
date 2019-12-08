2c726ef5ef89a5f95eb824cd2c72d8dd238c8415
mcaci <michele.caci@gmail.com>
2019-12-08 21:26:47 +0100

    update card sort code

--- app/msg/cardsort/sort.go
+++ app/msg/cardsort/sort.go
@@ -3,7 +3,6 @@ package cardsort
 import (
 	"github.com/mcaci/ita-cards/card"
 	"github.com/mcaci/ita-cards/set"
-	"github.com/mcaci/msdb5/dom/briscola"
 )
 
 type SortedCard struct {
@@ -18,18 +17,21 @@ func NewSorted(cards set.Cards, b *card.Seed) *SortedCard {
 func (ids SortedCard) Len() int { return len(ids.cards) }
 
 func (ids SortedCard) Less(i, j int) bool {
-	first, other := ids.cards[i], ids.cards[j]
-	isSameSeed := first.Seed() == other.Seed()
-	if ids.briscola == nil {
-		isOtherGreaterOnPoints := briscola.Points(first) < briscola.Points(other)
-		isSamePoints := briscola.Points(first) == briscola.Points(other)
-		isOtherGreaterOnNumber := first.Number() < other.Number()
-		return !(isSameSeed && ((isSamePoints && isOtherGreaterOnNumber) || isOtherGreaterOnPoints))
+	all := []uint8{1, 3, 10, 9, 8, 7, 6, 5, 4, 2,
+		11, 13, 20, 19, 18, 17, 16, 15, 14, 12,
+		21, 23, 30, 29, 28, 27, 26, 25, 24, 22,
+		31, 33, 40, 39, 38, 37, 36, 35, 34, 32}
+	iID, jID := ids.cards[i].ToID(), ids.cards[j].ToID()
+	var iIdx, jIdx int
+	for idx, cID := range all {
+		if iID == cID {
+			iIdx = idx
+		}
+		if jID == cID {
+			jIdx = idx
+		}
 	}
-	isOtherGreaterOnPoints := briscola.Points(first) < briscola.Points(other)
-	isSamePoints := briscola.Points(first) == briscola.Points(other)
-	isOtherGreaterOnNumber := first.Number() < other.Number()
-	return !(isSameSeed && ((isSamePoints && isOtherGreaterOnNumber) || isOtherGreaterOnPoints))
+	return iIdx <= jIdx
 }
 
 func (ids SortedCard) Swap(i, j int) { ids.cards[i], ids.cards[j] = ids.cards[j], ids.cards[i] }
