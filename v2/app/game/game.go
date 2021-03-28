3e3da5eec303b06bda14c9509fa2cc51c57443f3
mcaci <michele.caci@gmail.com>
2021-03-28 22:41:15 +0200

    Remove unused fields and methods from game struct

--- v2/app/game/game.go
+++ v2/app/game/game.go
@@ -1,26 +1,22 @@
 package game
 
 import (
-	"container/list"
 	"fmt"
 
 	"github.com/mcaci/ita-cards/card"
 	"github.com/mcaci/ita-cards/set"
 	"github.com/mcaci/msdb5/v2/dom/auction"
-	"github.com/mcaci/msdb5/v2/dom/phase"
 	"github.com/mcaci/msdb5/v2/dom/player"
 	"github.com/mcaci/msdb5/v2/dom/team"
 )
 
 // Game struct
 type Game struct {
-	lastPlaying       list.List
 	players           team.Players
 	c                 callers
 	briscolaCard      card.Item
 	side, playedCards set.Cards
 	auctionScore      auction.Score
-	phase             phase.ID
 	opts              *Options
 }
 
@@ -31,26 +27,13 @@ type Options struct {
 func NewGame(gOpts *Options) *Game { return &Game{opts: gOpts} }
 
 // New func
-func New() *Game                              { return &Game{} }
-func (g *Game) Phase() phase.ID               { return g.phase }
-func (g *Game) CurrentPlayer() *player.Player { return g.lastPlaying.Front().Value.(*player.Player) }
-func (g *Game) LastPlayer() *player.Player    { return g.lastPlaying.Back().Value.(*player.Player) }
-func (g *Game) IsRoundOngoing() bool          { return len(g.playedCards) < 5 }
-func (g *Game) CurrentPlayerIndex() uint8 {
-	for i := range g.players {
-		if g.players[i] != g.CurrentPlayer() {
-			continue
-		}
-		return uint8(i)
-	}
-	return 0
-}
+func New() *Game { return &Game{} }
 
 func IsRoundOngoing(playedCards set.Cards) bool { return len(playedCards) < 5 }
 
 func (g Game) String() string {
-	return fmt.Sprintf("(Caller is: %s,\n Companion is: %s,\n Played cards: %v,\n Auction score: %d,\n Phase: %s,\n Players: %v,\n Side Deck: %v,\n Last Players: %v)",
-		g.c.caller.Name(), g.c.companion.Name()+" "+g.briscolaCard.String(), g.playedCards, g.auctionScore, g.phase, g.players, g.side, g.lastPlaying)
+	return fmt.Sprintf("(Caller is: %s,\n Companion is: %s,\n Played cards: %v,\n Auction score: %d,\n Players: %v,\n Side Deck: %v)",
+		g.c.caller.Name(), g.c.companion.Name()+" "+g.briscolaCard.String(), g.playedCards, g.auctionScore, g.players, g.side)
 }
 
 type callers struct {
