b0305ac70f0b7046f03a3aeefb3ff8e39a27f1f6
Michele Caci <michele.caci@gmail.com>
2021-04-02 19:24:11 +0200

    Add score call via grpc

---
+++ v2/README.md
@@ -0,0 +1,3 @@
+# msdb5
+
+Run docker containers for dom/briscola and dom/briscola5, then go run main.go
---
+++ v2/dom/briscola/srv/server.go
@@ -0,0 +1,32 @@
+package srv
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/mcaci/ita-cards/card"
+	"github.com/mcaci/msdb5/v2/dom/briscola"
+	"github.com/mcaci/msdb5/v2/pb"
+)
+
+type server struct {
+	pb.UnimplementedBriscolaServer
+}
+
+func NewServer() *server { return &server{} }
+
+func (s *server) Points(ctx context.Context, n *pb.NumberReq) (*pb.PointsRes, error) {
+	c, err := card.FromID(uint8(n.Number))
+	if err != nil {
+		return &pb.PointsRes{}, err
+	}
+	return &pb.PointsRes{Points: uint32(briscola.Points(c))}, nil
+}
+
+func (s *server) Score(ctx context.Context, cs *pb.Cards) (*pb.PointsRes, error) {
+	scrs, err := toScorers(*cs)
+	if err != nil {
+		return nil, fmt.Errorf("error in the conversion of the input: %w", err)
+	}
+	return &pb.PointsRes{Points: uint32(briscola.FinalScore(scrs...))}, nil
+}
