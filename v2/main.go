c585c4a8dd474362d43141d6cea3aa912851fb08
Michele Caci <michele.caci@gmail.com>
2021-04-13 19:13:19 +0200

    Move client main in frw cmd to main

--- v2/main.go
+++ v2/main.go
@@ -3,13 +3,23 @@ package main
 import (
 	"flag"
 	"log"
+	"net/http"
 
 	"github.com/mcaci/msdb5/v2/app/game"
+	"github.com/mcaci/msdb5/v2/frw/srv"
 )
 
 func main() {
 	noSide := flag.Bool("no-side", false, "Flag to specify no side deck is to be used. Default: false (no side deck used)")
+	aiGame := flag.Bool("ai-game", false, "Flag to specify if ai plays the game (for tests). Default: false (webapp at :8080)")
 	flag.Parse()
+
+	if !*aiGame {
+		http.HandleFunc("/", srv.Home)
+		http.HandleFunc("/start/", srv.Start)
+		http.HandleFunc("/draw/", srv.Draw)
+		log.Fatal(http.ListenAndServe(":8080", nil))
+	}
 
 	// setup game
 	g := game.NewGame(&game.Options{
