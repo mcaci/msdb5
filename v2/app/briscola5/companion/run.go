27184de6be3acd726f3c46450cc37f01fb5429cc
Michele Caci <michele.caci@gmail.com>
2021-08-05 20:23:38 +0200

    Simplify companion package

--- v2/app/briscola5/companion/run.go
+++ v2/app/briscola5/companion/run.go
@@ -1,47 +1,66 @@
 package companion
 
 import (
+	"log"
+
 	"github.com/mcaci/ita-cards/card"
 	"github.com/mcaci/msdb5/v2/dom/briscola"
 	"github.com/mcaci/msdb5/v2/dom/player"
 	"github.com/mcaci/msdb5/v2/dom/team"
 )
 
-func Run(s struct {
-	ID      uint8
+type companionIn struct {
+	Player  player.Player
 	Players team.Players
-}) struct {
+}
+type companionOut struct {
 	Briscola  briscola.Card
 	Companion uint8
-} {
+}
+
+func Run(s companionIn) companionOut {
+	selectedCard := selectCardFromSeedThatHasMostCardsInHand(s.Player)
+	serie := briscola.Serie(selectedCard)
+	for _, c := range serie {
+		i, err := s.Players.Index(player.IsCardInHand(c))
+		switch {
+		case err != nil:
+			log.Printf("error: %v. Card %v is inside the side deck", err, c)
+		case s.Players[i] == s.Player:
+			log.Print("Player is self")
+		default:
+			return companionOut{
+				Briscola:  briscola.Card{Item: c},
+				Companion: i,
+			}
+		}
+	}
+	log.Printf("not expecting at this point to have issues. Fallback to player1 for now.")
+	return companionOut{}
+}
+
+func selectCardFromSeedThatHasMostCardsInHand(p player.Player) card.Item {
+	// count how many cards per seed
 	count := make(map[card.Seed]uint8)
-	for _, c := range *s.Players[s.ID].Hand() {
+	for _, c := range *p.Hand() {
 		count[c.Seed()]++
 	}
+	// check seed with highest card
 	var max uint8
 	var seed card.Seed
 	for k, v := range count {
 		if v < max {
 			continue
 		}
+		max = v
 		seed = k
 	}
-	serie := briscola.Serie(Seeder(seed))
-	for _, c := range serie {
-		i, err := s.Players.Index(player.IsCardInHand(c))
-		if err != nil {
-			// card is in side deck
+	// select card from seed
+	for _, c := range *p.Hand() {
+		if c.Seed() != seed {
 			continue
 		}
-		if i == s.ID {
-			// player is self
-			continue
-		}
-		return Round(briscola.Card{Item: c}, s.Players)
+		return c
 	}
-	return Round(*briscola.MustID(1), s.Players)
+	return card.Item{}
 }
-
-type Seeder card.Seed
-
-func (s Seeder) Seed() card.Seed { return card.Seed(s) }
---
+++ v2/app/briscola5/companion/run_test.go
@@ -0,0 +1,59 @@
+package companion
+
+import (
+	"testing"
+
+	"github.com/mcaci/ita-cards/card"
+	"github.com/mcaci/msdb5/v2/dom/briscola"
+	"github.com/mcaci/msdb5/v2/dom/team"
+)
+
+func TestRunMinimalExample(t *testing.T) {
+	in := companionIn{}
+	in.Players = *team.New(5)
+	in.Player = in.Players[0]
+	out := Run(in)
+	if out.Companion != 0 {
+		t.Errorf("Not possible that a companion was found: %d", out.Companion)
+	}
+	var card briscola.Card
+	if out.Briscola != card {
+		t.Errorf("Not possible that a card was found: %v", out.Briscola)
+	}
+}
+
+func TestRunExampleNominal(t *testing.T) {
+	in := companionIn{}
+	in.Players = *team.New(5)
+	in.Players[0].Hand().Add(*card.MustID(1), *card.MustID(1))
+	in.Players[1].Hand().Add(*card.MustID(11), *card.MustID(23))
+	in.Players[2].Hand().Add(*card.MustID(11), *card.MustID(22))
+	in.Players[3].Hand().Add(*card.MustID(21), *card.MustID(21))
+	in.Players[4].Hand().Add(*card.MustID(31), *card.MustID(31))
+	in.Player = in.Players[3]
+	out := Run(in)
+	if out.Companion != 1 {
+		t.Errorf("Not possible that a companion was found: %d", out.Companion)
+	}
+	if out.Briscola.ToID() != 23 {
+		t.Errorf("Not possible that a card was found: %v", out.Briscola)
+	}
+}
+
+func TestRunExampleAnother(t *testing.T) {
+	in := companionIn{}
+	in.Players = *team.New(5)
+	in.Players[0].Hand().Add(*card.MustID(1), *card.MustID(1), *card.MustID(1))
+	in.Players[1].Hand().Add(*card.MustID(11), *card.MustID(22), *card.MustID(11))
+	in.Players[2].Hand().Add(*card.MustID(23), *card.MustID(29), *card.MustID(11))
+	in.Players[3].Hand().Add(*card.MustID(21), *card.MustID(21), *card.MustID(11))
+	in.Players[4].Hand().Add(*card.MustID(31), *card.MustID(31), *card.MustID(31))
+	in.Player = in.Players[3]
+	out := Run(in)
+	if out.Companion != 2 {
+		t.Errorf("Not possible that a companion was found: %d", out.Companion)
+	}
+	if out.Briscola.ToID() != 23 {
+		t.Errorf("Not possible that a card was found: %v", out.Briscola)
+	}
+}
