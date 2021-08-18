eb5c0af17e1244724f2523775e323933d0cb4fcf
Michele Caci <michele.caci@gmail.com>
2021-08-18 18:45:30 +0200

    call srv from main

--- v2/main.go
+++ v2/main.go
@@ -4,13 +4,13 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/mcaci/msdb5/v2/frw/srv"
+	"github.com/mcaci/msdb5/v2/srvb"
 )
 
 func main() {
-	http.HandleFunc("/", srv.Home)
-	http.HandleFunc("/start/", srv.Start)
-	http.HandleFunc("/play/", srv.Play)
-	http.HandleFunc("/refresh/", srv.Refresh)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/create", srvb.Create)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
