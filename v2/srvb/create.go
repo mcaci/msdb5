aca490dad38c58da41f9c2d6fdc58cc54ba54d86
Michele Caci <michele.caci@gmail.com>
2021-08-24 20:04:20 +0200

    Add creation test cases

--- v2/srvb/create.go
+++ v2/srvb/create.go
@@ -1,18 +1,40 @@
 package srvb
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/mcaci/msdb5/v2/app/briscola"
 )
 
 func Create(w http.ResponseWriter, r *http.Request) {
-	g := briscola.NewGame(briscola.WithDefaultOptions)
+	opts, err := options(r)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+	g := briscola.NewGame(opts)
 	if g == nil {
 		http.Error(w, "game could not be created", http.StatusBadRequest)
 		return
 	}
-	w.Write([]byte("OK"))
+	json.NewEncoder(w).Encode(g)
+}
+
+func options(r *http.Request) (*briscola.Options, error) {
+	if r.Body == nil {
+		return briscola.WithDefaultOptions, nil
+	}
+	defer r.Body.Close()
+	var req struct {
+		Name string `json:"name"`
+	}
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		return nil, err
+	}
+	return &briscola.Options{WithName: req.Name}, nil
+
 }
 
 func Handler() http.Handler {
--- v2/srvb/create_test.go
+++ v2/srvb/create_test.go
@@ -1,64 +1,60 @@
 package srvb_test
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/mcaci/msdb5/v2/srvb"
 )
 
 func TestCreation(t *testing.T) {
-	req, err := http.NewRequest("GET", "localhost:8080/create", nil)
-	if err != nil {
-		t.Fatalf("could not create request: %v", err)
+	const url = "localhost:8080/create"
+	td := []struct {
+		name     string
+		method   string
+		reqBody  io.Reader
+		expected string
+	}{
+		{"Default game creation", "GET", nil, ""},
+		{"Game creation with name", "POST", strings.NewReader(fmt.Sprintf(`{"name":"%s"}`, "newgame")), "newgame"},
+		{"Game creation with name and GET", "GET", strings.NewReader(fmt.Sprintf(`{"name":"%s"}`, "abc")), "abc"},
 	}
-	rec := httptest.NewRecorder()
-	srvb.Create(rec, req)
+	for _, tc := range td {
+		t.Run(tc.name, func(t *testing.T) {
+			req, err := http.NewRequest(tc.method, url, tc.reqBody)
+			if err != nil {
+				t.Fatalf("could not create request: %v", err)
+			}
+			if tc.method == "POST" {
+				req.Header.Set("Content-Type", "application/json")
+			}
+			rec := httptest.NewRecorder()
+			srvb.Create(rec, req)
+			res := rec.Result()
+			defer res.Body.Close()
+			if res.StatusCode != http.StatusOK {
+				t.Errorf("expected status OK; got %v", res.StatusCode)
+			}
 
-	res := rec.Result()
-	defer res.Body.Close()
-	if res.StatusCode != http.StatusOK {
-		t.Errorf("expected status OK; got %v", res.StatusCode)
-	}
-
-	b, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		t.Fatalf("could not read response: %v", err)
-	}
-
-	expected := "OK"
-	if string(b) != expected {
-		t.Fatalf("expecting %v got %v", expected, string(b))
-	}
-}
-
-// TODO
-func TestCreationWithName(t *testing.T) {
-	req, err := http.NewRequest("GET", "localhost:8080/create?name=new", nil)
-	if err != nil {
-		t.Fatalf("could not create request: %v", err)
-	}
-	rec := httptest.NewRecorder()
-	srvb.Create(rec, req)
-
-	res := rec.Result()
-	defer res.Body.Close()
-	if res.StatusCode != http.StatusOK {
-		t.Errorf("expected status OK; got %v", res.StatusCode)
-	}
-
-	b, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		t.Fatalf("could not read response: %v", err)
+			var rs struct {
+				Name string `json:"name"`
+			}
+			rserr := json.NewDecoder(res.Body).Decode(&rs)
+			if rserr != nil {
+				t.Fatalf("%v", rserr)
+			}
+			if rs.Name != tc.expected {
+				t.Fatalf("expecting %v got %v", tc.expected, rs.Name)
+			}
+		})
 	}
 
-	expected := "OK"
-	if string(b) != expected {
-		t.Fatalf("expecting %v got %v", expected, string(b))
-	}
 }
 
 func TestRouting(t *testing.T) {
@@ -79,9 +75,8 @@ func TestRouting(t *testing.T) {
 		t.Fatalf("could not read response: %v", err)
 	}
 
-	expected := "OK"
+	expected := "EOF"
 	if string(b) != expected {
 		t.Fatalf("expecting %v got %v", expected, string(b))
 	}
-
 }
