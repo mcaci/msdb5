7485947b3f8ac792dfc1ed56062151a0d0c4dd57
Michele Caci <michele.caci@gmail.com>
2021-09-10 13:57:41 +0200

    rename setup to operation

--- v2/srvb/srvb_test.go
+++ v2/srvb/srvb_test.go
@@ -14,58 +14,58 @@ import (
 func TestSrvbOperations(t *testing.T) {
 	td := []struct {
 		name               string
-		setups             []setup
+		ops                []operation
 		expectedStatusCode int
 		expectedMsg        string
 		decoder            func(res *http.Response) (string, error)
 	}{
-		{"Creation with no body gives error", []setup{{body: nil, r: create}}, http.StatusBadRequest, "", koDec},
+		{"Creation with no body gives error", []operation{{body: nil, req: create}}, http.StatusBadRequest, "", koDec},
 		// curl -XPOST  -H "Content-Type: application/json" localhost:8080/create -d '{"name":"newgame"}'
-		{"Game creation with name", []setup{{body: strings.NewReader(fmt.Sprintf(`{"name":"%s"}`, "newgame")), r: create}}, http.StatusOK, "newgame", creationDec},
-		{"Game creation with error", []setup{{body: strings.NewReader(`'{"name":"na"}`), r: create}}, http.StatusInternalServerError, "could not process the request", koDec},
-		{"Cannot create two games", []setup{
-			{body: strings.NewReader(fmt.Sprintf(`{"name":"%s"}`, "gg")), r: create},
-			{body: strings.NewReader(`{"name":"errgame"}`), r: create},
+		{"Game creation with name", []operation{{body: strings.NewReader(fmt.Sprintf(`{"name":"%s"}`, "newgame")), req: create}}, http.StatusOK, "newgame", creationDec},
+		{"Game creation with error", []operation{{body: strings.NewReader(`'{"name":"na"}`), req: create}}, http.StatusInternalServerError, "could not process the request", koDec},
+		{"Cannot create two games", []operation{
+			{body: strings.NewReader(fmt.Sprintf(`{"name":"%s"}`, "gg")), req: create},
+			{body: strings.NewReader(`{"name":"errgame"}`), req: create},
 		}, http.StatusInternalServerError, "one game already created, cannot create more", koDec},
-		{"Join with no body gives error", []setup{{body: nil, r: join}}, http.StatusBadRequest, "empty request", koDec},
-		{"Join with wrong body gives error", []setup{{body: strings.NewReader(`'{"name":"na"}`), r: join}}, http.StatusBadRequest, "could not process the request", koDec},
-		{"Join with no create gives error", []setup{
-			{body: strings.NewReader(fmt.Sprintf(`{"name":"%s","game":"%s"}`, "mary", "newgame")), r: join},
+		{"Join with no body gives error", []operation{{body: nil, req: join}}, http.StatusBadRequest, "empty request", koDec},
+		{"Join with wrong body gives error", []operation{{body: strings.NewReader(`'{"name":"na"}`), req: join}}, http.StatusBadRequest, "could not process the request", koDec},
+		{"Join with no create gives error", []operation{
+			{body: strings.NewReader(fmt.Sprintf(`{"name":"%s","game":"%s"}`, "mary", "newgame")), req: join},
 		}, http.StatusInternalServerError, "not created", koDec},
-		{"Join on wrong game", []setup{
-			{body: strings.NewReader(fmt.Sprintf(`{"name":"%s"}`, "newgame")), r: create},
-			{body: strings.NewReader(fmt.Sprintf(`{"name":"%s","game":"%s"}`, "mary", "othergame")), r: join},
+		{"Join on wrong game", []operation{
+			{body: strings.NewReader(fmt.Sprintf(`{"name":"%s"}`, "newgame")), req: create},
+			{body: strings.NewReader(fmt.Sprintf(`{"name":"%s","game":"%s"}`, "mary", "othergame")), req: join},
 		}, http.StatusInternalServerError, "different name", koDec},
-		{"Join with no player name gives error", []setup{
-			{body: strings.NewReader(fmt.Sprintf(`{"name":"%s"}`, "newgame")), r: create},
-			{body: strings.NewReader(`{"game":"newgame"}`), r: join},
+		{"Join with no player name gives error", []operation{
+			{body: strings.NewReader(fmt.Sprintf(`{"name":"%s"}`, "newgame")), req: create},
+			{body: strings.NewReader(`{"game":"newgame"}`), req: join},
 		}, http.StatusInternalServerError, "no player name was given", koDec},
-		{"Join with no game name gives error", []setup{
-			{body: strings.NewReader(fmt.Sprintf(`{"name":"%s"}`, "newgame")), r: create},
-			{body: strings.NewReader(`{"name":"player"}`), r: join},
+		{"Join with no game name gives error", []operation{
+			{body: strings.NewReader(fmt.Sprintf(`{"name":"%s"}`, "newgame")), req: create},
+			{body: strings.NewReader(`{"name":"player"}`), req: join},
 		}, http.StatusInternalServerError, "no game name was given", koDec},
-		{"Join with game and player name", []setup{
-			{body: strings.NewReader(fmt.Sprintf(`{"name":"%s"}`, "newgame")), r: create},
-			{body: strings.NewReader(fmt.Sprintf(`{"name":"%s","game":"%s"}`, "mary", "newgame")), r: join},
+		{"Join with game and player name", []operation{
+			{body: strings.NewReader(fmt.Sprintf(`{"name":"%s"}`, "newgame")), req: create},
+			{body: strings.NewReader(fmt.Sprintf(`{"name":"%s","game":"%s"}`, "mary", "newgame")), req: join},
 		}, http.StatusOK, "1", joinDec},
-		{"Two players join", []setup{
-			{body: strings.NewReader(fmt.Sprintf(`{"name":"%s"}`, "newgame")), r: create},
-			{body: strings.NewReader(fmt.Sprintf(`{"name":"%s","game":"%s"}`, "mary", "newgame")), r: join},
-			{body: strings.NewReader(fmt.Sprintf(`{"name":"%s","game":"%s"}`, "michi", "newgame")), r: join},
+		{"Two players join", []operation{
+			{body: strings.NewReader(fmt.Sprintf(`{"name":"%s"}`, "newgame")), req: create},
+			{body: strings.NewReader(fmt.Sprintf(`{"name":"%s","game":"%s"}`, "mary", "newgame")), req: join},
+			{body: strings.NewReader(fmt.Sprintf(`{"name":"%s","game":"%s"}`, "michi", "newgame")), req: join},
 		}, http.StatusOK, "2", joinDec},
-		{"Three players joining gives error", []setup{
-			{body: strings.NewReader(fmt.Sprintf(`{"name":"%s"}`, "newgame")), r: create},
-			{body: strings.NewReader(fmt.Sprintf(`{"name":"%s","game":"%s"}`, "mary", "newgame")), r: join},
-			{body: strings.NewReader(fmt.Sprintf(`{"name":"%s","game":"%s"}`, "michi", "newgame")), r: join},
-			{body: strings.NewReader(fmt.Sprintf(`{"name":"%s","game":"%s"}`, "onemore", "newgame")), r: join},
+		{"Three players joining gives error", []operation{
+			{body: strings.NewReader(fmt.Sprintf(`{"name":"%s"}`, "newgame")), req: create},
+			{body: strings.NewReader(fmt.Sprintf(`{"name":"%s","game":"%s"}`, "mary", "newgame")), req: join},
+			{body: strings.NewReader(fmt.Sprintf(`{"name":"%s","game":"%s"}`, "michi", "newgame")), req: join},
+			{body: strings.NewReader(fmt.Sprintf(`{"name":"%s","game":"%s"}`, "onemore", "newgame")), req: join},
 		}, http.StatusInternalServerError, "max players reached", koDec},
 	}
 	for _, tc := range td {
 		tester := testResWith(tc.expectedStatusCode, tc.decoder)
 		t.Run(tc.name, func(t *testing.T) {
-			res, err := exec(tc.setups...)
+			res, err := exec(tc.ops...)
 			if err != nil {
-				t.Errorf("could perform the setup: %v", err)
+				t.Errorf("could perform the operation: %v", err)
 			}
 			defer res.Body.Close()
 			if err := tester(res, tc.expectedMsg); err != nil {
@@ -76,18 +76,18 @@ func TestSrvbOperations(t *testing.T) {
 	}
 }
 
-type setup struct {
+type operation struct {
 	body io.Reader
-	r    interface {
+	req  interface {
 		url() string
 		send(*http.Request, error) (*http.Response, error)
 	}
 }
 
-func exec(s ...setup) (*http.Response, error) {
+func exec(ops ...operation) (*http.Response, error) {
 	var res *http.Response
-	for _, el := range s {
-		r, err := el.r.send(http.NewRequest(http.MethodPost, el.r.url(), el.body))
+	for _, op := range ops {
+		r, err := op.req.send(http.NewRequest(http.MethodPost, op.req.url(), op.body))
 		if err != nil {
 			return nil, err
 		}
